49deeaf1686bb18e5e1083f54ae85d22fba20b5e
Brian Avants <stnava@gmail.com>
2009-07-21 19:43:35 +0000

    update guided reg

--- Scripts/guidedregistration.sh
+++ Scripts/guidedregistration.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 if [ $# -lt 5  ]
 then
-echo " USAGE \n  sh command.sh  fixed.nii fixedhipp.nii  moving.nii movinghipp.nii outputname "
+echo " USAGE \n  sh command.sh  fixed.nii fixedhipp.nii  moving.nii movinghipp.nii outputname  iterations "
 echo " the template = fixed.nii   ,   the individual = moving.nii "
+echo " iterations should be of the form  100x100x10 "
 exit
 fi
 
@@ -11,8 +12,30 @@ FIXH=$2
 MOV=$3
 MOVH=$4
 OUT=$5
+ITS=$6
 
-ANTSPATH='/Users/stnava/Code/bin/ants/'
+#ANTSPATH='/Users/stnava/Code/bin/ants/'
+
+if  [ ${#FIX} -lt 1 -o  ! -f $FIX ]
+then
+echo " Problem with specified Fixed Image => User Specified Value = $FIX "
+exit
+fi
+if  [ ${#MOV} -lt 1 -o  ! -f $MOV ]
+then
+echo " Problem with specified Moving Image => User Specified Value = $MOV "
+exit
+fi
+if  [ ${#FIXH} -lt 1 -o  ! -f $FIXH ]
+then
+echo " Problem with specified Fixed Label Image => User Specified Value = $FIXH "
+exit
+fi
+if  [ ${#MOVH} -lt 1 -o  ! -f $MOVH ]
+then
+echo " Problem with specified Moving Label Image => User Specified Value = $MOVH "
+exit
+fi
 
 #  == Important Parameters Begin ==
 
@@ -20,16 +43,20 @@ LMWT=0.5  # weight on landmarks
 
 INTWT=1    # weight on intensity --  twice the landmarks
 
-LM=PSE[${FIX},${MOV},$FIXH,$MOVH,${LMWT},0.1,100,0,25,10]
+# PSE/point-set-expectation/PointSetExpectation[fixedImage,movingImage,fixedPoints,movingPoints,weight,pointSetPercentage,pointSetSigma,boundaryPointsOnly,kNeighborhood, PartialMatchingIterations=100000]
+# the partial matching option assumes the complete labeling is in the first set of label parameters ...
+# more iterations leads to more symmetry in the matching  - 0 iterations means full asymmetry
+PCT=0.1 # percent of labeled voxels to use
+PARZ=100 # PARZEN sigma
+LM=PSE[${FIX},${MOV},$FIXH,$MOVH,${LMWT},${PCT},${PARZ},0,25,100]
 
-INTENSITY=PR[$FIX,${MOV},${INTWT},2]
+INTENSITY=PR[$FIX,${MOV},${INTWT},4]
 
 #  == Important Parameters end? ==
 
- ${ANTSPATH}ANTS 3  -o $OUT  -i 55x50x30 -t SyN[1.5]  -r Gauss[3,0] -m $INTENSITY   -m   $LM
+ ${ANTSPATH}ANTS 3  -o $OUT  -i $ITS -t SyN[0.25]  -r Gauss[3,0] -m $INTENSITY   -m   $LM
 
  ${ANTSPATH}WarpImageMultiTransform 3 $MOV ${OUT}toTemplate.nii ${OUT}Warp.nii ${OUT}Affine.txt  -R $FIX
-
 
  ${ANTSPATH}WarpImageMultiTransform 3  $FIX ${OUT}toMov.nii -i ${OUT}Affine.txt  ${OUT}InverseWarp.nii  -R $MOV
 
