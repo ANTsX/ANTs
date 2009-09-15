6641e2500a8345c74d3ac127a371ec284874d122
Brian Avants <stnava@gmail.com>
2009-09-15 19:25:52 +0000

    update naming

--- Scripts/optimalsmooth.sh
+++ Scripts/optimalsmooth.sh
@@ -1,14 +1,17 @@
+#!/bin/sh
 
-
-# usage :  SurfaceBasedSmoothing ImageToSmooth  sigma SurfaceImage  outname  {numrepeatsofsmoothing}
+echo  " sh $0 outputprefix "
+if [ $# -lt 1 ] ; then
+exit
+fi
 
 export ANTSPATH=${ANTSPATH:="$HOME/bin/ants/"}
 SUB=$1
 TH=${SUB}thicknorm.nii.gz
 JA=${SUB}logjacobian.nii
 MK=${SUB}mask.nii.gz
-OUTT=s${SUB}thick.nii.gz
-OUTJ=s${SUB}jac.nii.gz
+OUTT=${SUB}smooththick.nii.gz
+OUTJ=${SUB}smoothjac.nii.gz
 REPS=5
   ${ANTSPATH}SurfaceBasedSmoothing $TH 1.5 $MK $OUTT $REPS
   ${ANTSPATH}SurfaceBasedSmoothing $JA 1.5 $MK $OUTJ $REPS
