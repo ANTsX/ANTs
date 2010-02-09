090177025a37ab682f2c40d433a0ce03dd9a0bfa
Brian Avants <stnava@gmail.com>
2010-02-09 22:10:07 +0000

    cross corr local metric correlation instability

--- ImageRegistration/itkCrossCorrelationRegistrationFunction.cxx
+++ ImageRegistration/itkCrossCorrelationRegistrationFunction.cxx
@@ -340,23 +340,12 @@ CrossCorrelationRegistrationFunction<TFixedImage, TMovingImage, TDeformationFiel
     //	    derivinv[qq]-=2.0*sfm/(sff*smm)*( Ii - sfm/smm*Ji )*gradJ[qq];
     }
 
-  if( sff * smm != 0.0 )
+  float localCrossCorrelation = 0;
+  if( sff * smm > 1.e-5 )
     {
     localCrossCorrelation = sfm * sfm / ( sff * smm );
     }
-  else if( sff == 0.0 && smm == 0 )
-    {
-    localCrossCorrelation = 1.0;
-    }
-  else
-    {
-    localCrossCorrelation = 1.0;
-    }
-  if( localCrossCorrelation * (-1.0) < this->m_RobustnessParameter )
-    {
-    deriv.Fill(0);
-    }
-
+  //  if ( localCrossCorrelation*(-1.0) < this->m_RobustnessParameter) deriv.Fill(0);
 //  if ( localCrossCorrelation*(-1.0) < this->m_RobustnessParameter) {
 //  std::cout << " localC " << localCrossCorrelation << std::endl; }
 
