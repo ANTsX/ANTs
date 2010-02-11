65e0bcd29b7834e97712ee8301a23842bd0f9881
Nick Tustison <ntustison@gmail.com>
2010-02-11 04:14:41 +0000

    ENH:  Speed up of the calculation of the metric in InitializeIteration() by maintaining a queue where the sums within the hyperplanes orthogonal to the direction of iteration are kept.  Still need to perform timing checks.

--- ImageRegistration/itkCrossCorrelationRegistrationFunction.cxx
+++ ImageRegistration/itkCrossCorrelationRegistrationFunction.cxx
@@ -21,10 +21,14 @@
 #include "itkExceptionObject.h"
 #include "vnl/vnl_math.h"
 #include "itkImageFileWriter.h"
+#include "itkImageLinearConstIteratorWithIndex.h"
 #include "itkDiscreteGaussianImageFilter.h"
 #include "itkMeanImageFilter.h"
 #include "itkMedianImageFilter.h"
 #include "itkImageFileWriter.h"
+
+#include <deque>
+
 namespace itk
 {
 /*
@@ -188,93 +192,198 @@ CrossCorrelationRegistrationFunction<TFixedImage, TMovingImage, TDeformationFiel
 
   // compute local means
   //  typedef itk::ImageRegionIteratorWithIndex<MetricImageType> Iterator;
-  Iterator outIter(this->finitediffimages[0], this->finitediffimages[0]->GetLargestPossibleRegion() );
-  for( outIter.GoToBegin(); !outIter.IsAtEnd(); ++outIter )
+
+  //
+  // The following change was made to speed up the correlation calculation.
+  //
+
+  typedef std::deque<float> SumQueueType;
+
+  SumQueueType Qsuma2;
+  SumQueueType Qsumb2;
+  SumQueueType Qsuma;
+  SumQueueType Qsumb;
+  SumQueueType Qsumab;
+  SumQueueType Qcount;
+
+  ImageLinearConstIteratorWithIndex<MetricImageType> outIter( this->finitediffimages[0],
+                                                              this->finitediffimages[0]->GetLargestPossibleRegion() );
+  outIter.SetDirection( 0 );
+  outIter.GoToBegin();
+  while( !outIter.IsAtEnd() )
     {
-    bool takesample = true;
-    if( this->m_FixedImageMask )
+    // Push the zeros onto the stack that are outsized the image boundary at
+    // the beginning of the line.
+    Qsuma2 = SumQueueType( r[0], 0.0 );
+    Qsumb2 = SumQueueType( r[0], 0.0 );
+    Qsuma = SumQueueType( r[0], 0.0 );
+    Qsumb = SumQueueType( r[0], 0.0 );
+    Qsumab = SumQueueType( r[0], 0.0 );
+    Qcount = SumQueueType( r[0], 0.0 );
+
+    NeighborhoodIterator<MetricImageType> hoodIt(
+      this->GetRadius(),
+      this->finitediffimages[0],
+      this->finitediffimages[0]->GetLargestPossibleRegion() );
+    IndexType oindex = outIter.GetIndex();
+    hoodIt.SetLocation( oindex );
+    unsigned int hoodlen = hoodIt.Size();
+    // Now add the rest of the values from each hyperplane
+    for( unsigned int i = r[0]; i < ( 2 * r[0] + 1 ); i++ )
       {
-      if( this->m_FixedImageMask->GetPixel( outIter.GetIndex() ) < 0.25 )
+      float suma2 = 0.0;
+      float sumb2 = 0.0;
+      float suma = 0.0;
+      float sumb = 0.0;
+      float sumab = 0.0;
+      float count = 0.0;
+      for( unsigned int indct = i; indct < hoodlen; indct += ( 2 * r[0] + 1 ) )
         {
-        takesample = false;
+        bool isInBounds = true;
+        hoodIt.GetPixel( indct, isInBounds );
+        IndexType index = hoodIt.GetIndex( indct );
+
+        if( !isInBounds || ( this->m_FixedImageMask &&
+                             this->m_FixedImageMask->GetPixel( index ) < 0.25 ) )
+          {
+          continue;
+          }
+
+        float a = this->GetFixedImage()->GetPixel( index );
+        float b = this->GetMovingImage()->GetPixel( index );
+
+        suma2 += a * a;
+        sumb2 += b * b;
+        suma += a;
+        sumb += b;
+        sumab += a * b;
+        count += 1.0;
         }
+
+      Qsuma2.push_back( suma2 );
+      Qsumb2.push_back( sumb2 );
+      Qsuma.push_back( suma );
+      Qsumb.push_back( sumb );
+      Qsumab.push_back( sumab );
+      Qcount.push_back( count );
       }
 
-    if( takesample )
+    while( !outIter.IsAtEndOfLine() )
       {
-      NeighborhoodIterator<MetricImageType>
-      hoodIt(
-        this->GetRadius(), this->finitediffimages[0], this->finitediffimages[0]->GetLargestPossibleRegion() );
-      IndexType oindex = outIter.GetIndex();
-      hoodIt.SetLocation(oindex);
+      // Test to see if there are any voxels we need to handle in the current
+      // window.
 
-      double fixedMean = 0;
-      double movingMean = 0;
+      float suma2 = 0.0;
+      float sumb2 = 0.0;
+      float suma = 0.0;
+      float sumb = 0.0;
+      float sumab = 0.0;
+      float count = 0.0;
 
-      PointType    mappedPoint;
-      unsigned int indct;
-      unsigned int hoodlen = hoodIt.Size();
-
-      //      unsigned int inct=0;
-      double       sff = 0, smm = 0, sfm = 0;
-      unsigned int cter = 0;
-      double       asq = 0, bsq = 0, sumai = 0, sumbi = 0, sumaibi = 0;
-      for( indct = 0; indct < hoodlen; indct++ )
+      typename SumQueueType::iterator itcount = Qcount.begin();
+      while( itcount != Qcount.end() )
         {
-        IndexType index = hoodIt.GetIndex(indct);
-        bool      inimage = true;
-        for( unsigned int dd = 0; dd < ImageDimension; dd++ )
-          {
-          if( index[dd] < 0 || index[dd] > static_cast<typename IndexType::IndexValueType>(imagesize[dd] - 1) )
-            {
-            inimage = false;
-            }
-          }
-        if( inimage && this->m_FixedImageMask )
-          {
-          if( this->m_FixedImageMask->GetPixel( index ) < 0.25 )
-            {
-            inimage = false;
-            }
-          }
-        if( inimage )
-          {
-          float ff = this->GetFixedImage()->GetPixel(index);
-          float gg = this->GetMovingImage()->GetPixel(index);
-          asq += ff * ff;
-          bsq += gg * gg;
-          sumaibi += ff * gg;
-          sumbi += gg;
-          sumai += ff;
-          cter++;
-          }
+        count += *itcount;
+        ++itcount;
         }
 
-      if( cter > 0  )
+      // If there are values, we need to calculate the different quantities
+      if( count > 0 )
         {
-        fixedMean = sumai / (float)cter;
-        movingMean = sumbi / (float)cter;
+        typename SumQueueType::iterator ita2 = Qsuma2.begin();
+        typename SumQueueType::iterator itb2 = Qsumb2.begin();
+        typename SumQueueType::iterator ita = Qsuma.begin();
+        typename SumQueueType::iterator itb = Qsumb.begin();
+        typename SumQueueType::iterator itab = Qsumab.begin();
 
-        sff = asq - fixedMean * sumai - fixedMean * sumai + cter * fixedMean * fixedMean;
-        smm = bsq - movingMean * sumbi - movingMean * sumbi + cter * movingMean * movingMean;
-        sfm = sumaibi - movingMean * sumai - fixedMean * sumbi + cter * movingMean * fixedMean;
-        // sff/=(float)sff;
-        // sfm/=(float)sfm;
-        // smm/=(float)smm;
+        while( ita2 != Qsuma2.end() )
+          {
+          suma2 += *ita2;
+          sumb2 += *itb2;
+          suma += *ita;
+          sumb += *itb;
+          sumab += *itab;
 
-        float val = this->GetFixedImage()->GetPixel(oindex) - fixedMean;
+          ++ita2;
+          ++itb2;
+          ++ita;
+          ++itb;
+          ++itab;
+          }
+
+        float fixedMean = suma / count;
+        float movingMean = sumb / count;
+
+        float sff = suma2 - fixedMean * suma - fixedMean * suma + count * fixedMean * fixedMean;
+        float smm = sumb2 - movingMean * sumb - movingMean * sumb + count * movingMean * movingMean;
+        float sfm = sumab - movingMean * suma - fixedMean * sumb + count * movingMean * fixedMean;
+
+        IndexType oindex = outIter.GetIndex();
+
+        float val = this->GetFixedImage()->GetPixel( oindex ) - fixedMean;
         this->finitediffimages[0]->SetPixel( oindex, val );
-        val = this->GetMovingImage()->GetPixel(oindex) - movingMean;
+        val = this->GetMovingImage()->GetPixel( oindex ) - movingMean;
         this->finitediffimages[1]->SetPixel( oindex, val );
         this->finitediffimages[2]->SetPixel( oindex, sfm ); // A
         this->finitediffimages[3]->SetPixel( oindex, sff ); // B
         this->finitediffimages[4]->SetPixel( oindex, smm ); // C
         }
 
-      //	  std::cout << oindex << " NM1 " << this->GetFixedImage()->GetPixel(oindex) - fixedMean << " NM2 " <<
-      // this->GetMovingImage()->GetPixel(oindex) - movingMean << std::endl;
-      //	  std::cout << " sff " << sff << " sfm " << sfm << " smm " << smm << std::endl;
+      // Increment the iterator and check to see if we're at the end of the
+      // line.  If so, go to the next line.  Otherwise, add the
+      // the values for the next hyperplane.
+      ++outIter;
+
+      if( !outIter.IsAtEndOfLine() )
+        {
+        hoodIt.SetLocation( outIter.GetIndex() );
+
+        suma2 = 0.0;
+        sumb2 = 0.0;
+        suma = 0.0;
+        sumb = 0.0;
+        sumab = 0.0;
+        count = 0.0;
+        for( unsigned int indct = 2 * r[0]; indct < hoodlen; indct += ( 2 * r[0] + 1 ) )
+          {
+          bool isInBounds = true;
+          hoodIt.GetPixel( indct, isInBounds );
+          IndexType index = hoodIt.GetIndex( indct );
+
+          if( !isInBounds || ( this->m_FixedImageMask &&
+                               this->m_FixedImageMask->GetPixel( index ) < 0.25 ) )
+            {
+            continue;
+            }
+
+          float a = this->GetFixedImage()->GetPixel( index );
+          float b = this->GetMovingImage()->GetPixel( index );
+
+          suma2 += a * a;
+          sumb2 += b * b;
+          suma += a;
+          sumb += b;
+          sumab += a * b;
+          count += 1.0;
+          }
+
+        Qsuma2.push_back( suma2 );
+        Qsumb2.push_back( sumb2 );
+        Qsuma.push_back( suma );
+        Qsumb.push_back( sumb );
+        Qsumab.push_back( sumab );
+        Qcount.push_back( count );
+        }
+
+      Qsuma2.pop_front();
+      Qsumb2.pop_front();
+      Qsuma.pop_front();
+      Qsumb.pop_front();
+      Qsumab.pop_front();
+      Qcount.pop_front();
       }
+
+    outIter.NextLine();
     }
 
   // m_FixedImageGradientCalculator->SetInputImage(finitediffimages[0]);
