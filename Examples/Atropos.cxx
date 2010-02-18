d95b75d0f7302d823a9db1844385717fb05a324a
Nick Tustison <ntustison@gmail.com>
2010-02-18 15:21:43 +0000

    BUG?:  Fixing the command line to ensure default values are not overridden if the corresponding flag is not specified.

--- Examples/Atropos.cxx
+++ Examples/Atropos.cxx
@@ -222,7 +222,7 @@ int AtroposSegmentation( itk::CommandLineParser *parser )
    */
   typename itk::CommandLineParser::OptionType::Pointer iterationsOption =
     parser->GetOption( "number-of-iterations" );
-  if( iterationsOption )
+  if( iterationsOption && iterationsOption->GetNumberOfValues() > 0 )
     {
     segmenter->SetMaximumNumberOfIterations( parser->Convert<unsigned int>(
                                                iterationsOption->GetValue() ) );
@@ -233,7 +233,7 @@ int AtroposSegmentation( itk::CommandLineParser *parser )
    */
   typename itk::CommandLineParser::OptionType::Pointer convergenceOption =
     parser->GetOption( "convergence-threshold" );
-  if( convergenceOption )
+  if( convergenceOption && convergenceOption->GetNumberOfValues() > 0 )
     {
     segmenter->SetConvergenceThreshold( parser->Convert<float>(
                                           convergenceOption->GetValue() ) );
@@ -244,7 +244,7 @@ int AtroposSegmentation( itk::CommandLineParser *parser )
    */
   typename itk::CommandLineParser::OptionType::Pointer maskOption =
     parser->GetOption( "mask-image" );
-  if( maskOption && maskOption->GetNumberOfValues() )
+  if( maskOption && maskOption->GetNumberOfValues() > 0 )
     {
     try
       {
@@ -389,7 +389,7 @@ int AtroposSegmentation( itk::CommandLineParser *parser )
    */
   typename itk::CommandLineParser::OptionType::Pointer mrfOption =
     parser->GetOption( "mrf" );
-  if( mrfOption && mrfOption->GetNumberOfValues() )
+  if( mrfOption && mrfOption->GetNumberOfValues() > 0 )
     {
     if( mrfOption->GetNumberOfParameters() > 0 )
       {
@@ -437,7 +437,7 @@ int AtroposSegmentation( itk::CommandLineParser *parser )
    */
   typename itk::CommandLineParser::OptionType::Pointer distanceOption =
     parser->GetOption( "use-euclidean-distance" );
-  if( distanceOption )
+  if( distanceOption && distanceOption->GetNumberOfValues() > 0 )
     {
     segmenter->SetUseEuclideanDistanceForPriorLabels(
       parser->Convert<bool>( distanceOption->GetValue() ) );
@@ -448,7 +448,7 @@ int AtroposSegmentation( itk::CommandLineParser *parser )
    */
   typename itk::CommandLineParser::OptionType::Pointer memoryOption =
     parser->GetOption( "minimize-memory-usage" );
-  if( memoryOption )
+  if( memoryOption && memoryOption->GetNumberOfValues() > 0 )
     {
     segmenter->SetMinimizeMemoryUsage( parser->Convert<bool>(
                                          memoryOption->GetValue() ) );
@@ -469,7 +469,7 @@ int AtroposSegmentation( itk::CommandLineParser *parser )
    */
   typename itk::CommandLineParser::OptionType::Pointer outputOption =
     parser->GetOption( "output" );
-  if( outputOption )
+  if( outputOption && outputOption->GetNumberOfValues() > 0 )
     {
     if( outputOption->GetNumberOfParameters() == 0 )
       {
