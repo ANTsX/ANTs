5042516846d6953bcf24b60c8fcc14c6c96900f3
Brian Avants <stnava@gmail.com>
2010-03-14 20:01:29 +0000

    dont write out temporary results

--- Examples/KellySlater.cxx
+++ Examples/KellySlater.cxx
@@ -1292,9 +1292,6 @@ int LaplacianThicknessExpDiff2(int argc, char *argv[])
     std::cout << " error " << totalerr << " at it " << its  << " th-err " << thicknesserror / (float)thickerrct
               << " max thick " << maxth << std::endl;
 //    std::string sulcthickname =outname + "sulcthick.nii";
-    finalthickimage->SetDirection(omat);
-    WriteImage<ImageType>(finalthickimage, outname.c_str() );
-    finalthickimage->SetDirection(fmat);
     //    if (ImageDimension==2) WriteJpg<ImageType>(finalthickimage,"thick.jpg");
     //    std::string velofieldname = outname + "velofield";
     // WriteDisplacementField<DeformationFieldType>(velofield,velofieldname.c_str());
@@ -1304,6 +1301,10 @@ int LaplacianThicknessExpDiff2(int argc, char *argv[])
       }
     // std::cin.get();
     }
+
+  finalthickimage->SetDirection(omat);
+  WriteImage<ImageType>(finalthickimage, outname.c_str() );
+  finalthickimage->SetDirection(fmat);
 
   return 0;
 
