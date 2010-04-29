6ad3f59bed1e50d3def151739dc190d70c7cbe1d
Brian Avants <stnava@gmail.com>
2010-04-29 00:00:48 +0000

    changing the permissions of the files below using
    for x in *sh ; do  svn propset svn:executable ON  $x ; done

--- Scripts/antsdeformationmag.sh
+++ Scripts/antsdeformationmag.sh
@@ -14,37 +14,37 @@ inwarp=$1
 whichmag=$2
 magimage=$3
 ANTSPATH=$4
-dimarr=(`${ANTSPATH}PrintHeader ${inwarp}xvec.nii  | grep Spacing | grep , | wc `)
+dimarr=(`${ANTSPATH}PrintHeader ${inwarp}xvec.nii.gz  | grep Spacing | grep , | wc `)
 DIM=${dimarr[0]}
 let DIM=${DIM}+1
 # multiply some image of the right size by 0 to make the initial magnitude image
-${ANTSPATH}ImageMath  $DIM  $magimage m  ${inwarp}xvec.nii 0
-${ANTSPATH}ImageMath  $DIM  ${magimage}temp.nii m  ${inwarp}xvec.nii 0
+${ANTSPATH}ImageMath  $DIM  $magimage m  ${inwarp}xvec.nii.gz 0
+${ANTSPATH}ImageMath  $DIM  ${magimage}temp.nii.gz m  ${inwarp}xvec.nii.gz 0
 ok=0
 for  x in  x y z
 do
-if [ -s  ${inwarp}${x}vec.nii ]
+if [ -s  ${inwarp}${x}vec.nii.gz ]
 then
 # compute the magnitude of the x-component deformation
 if [ $whichmag == Laplacian ]
 then
 echo " Laplacian $x "
 ok=1
-${ANTSPATH}ImageMath  $DIM  ${magimage}temp.nii Laplacian  ${inwarp}${x}vec.nii 1.0 0
+${ANTSPATH}ImageMath  $DIM  ${magimage}temp.nii.gz Laplacian  ${inwarp}${x}vec.nii.gz 1.0 0
 elif [ $whichmag == Grad ]
 then
 echo " Grad $x "
 ok=1
-${ANTSPATH}ImageMath  $DIM  ${magimage}temp.nii Grad ${inwarp}${x}vec.nii 1.0  0
-#ImageMath  $DIM  temp.nii m temp.nii temp.nii
+${ANTSPATH}ImageMath  $DIM  ${magimage}temp.nii.gz Grad ${inwarp}${x}vec.nii.gz 1.0  0
+#ImageMath  $DIM  temp.nii.gz m temp.nii.gz temp.nii.gz
 elif [ $whichmag == Euclidean ]
 then
 echo " Euclidean $x "
 ok=1
-${ANTSPATH}ImageMath  $DIM  ${magimage}temp.nii m  ${inwarp}${x}vec.nii ${inwarp}${x}vec.nii
+${ANTSPATH}ImageMath  $DIM  ${magimage}temp.nii.gz m  ${inwarp}${x}vec.nii.gz ${inwarp}${x}vec.nii.gz
 fi
 # add the xvec magnitude to the magimage
-${ANTSPATH}ImageMath  $DIM  $magimage  +  $magimage ${magimage}temp.nii
+${ANTSPATH}ImageMath  $DIM  $magimage  +  $magimage ${magimage}temp.nii.gz
 fi # ifz
 done #loop
 if [ $ok -eq 1 ]
@@ -59,4 +59,4 @@ echo " User Requested deformation measure ::   $whichmag "
 echo " that is not available -- user needs to choose another type of deformation measure "
 fi
 
-rm -f  ${magimage}temp.nii
+rm -f  ${magimage}temp.nii.gz
