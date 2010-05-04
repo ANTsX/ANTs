8cfaba06467a5fea63dba9af605941fbfff8c74b
Brian Avants <stnava@gmail.com>
2010-05-04 20:37:36 +0000

    updated parameters

--- Scripts/landmarkmatch.sh
+++ Scripts/landmarkmatch.sh
@@ -2,6 +2,7 @@
 if [ $# -lt 6  ]
 then
 echo " USAGE \n  sh $0  fixed.nii fixedhipp.nii  moving.nii movinghipp.nii  ITERATIONS LandmarkWeight "
+echo " you should set LandmarkWeight yourself --- the intensity weight is 1.  so the landmark weight should be in that range (e.g. 0.5 => 1 ) "
 exit
 fi
 
@@ -39,21 +40,21 @@ dothis=0
 if [ $dothis -eq 1 ]
 then
 # here, we select a subset of the labeled regions
-FIXHMOD=${OUT2}locmod.nii
-MOVHMOD=${OUT}locmod.nii
+FIXHMOD=${OUT2}locmod.nii.gz
+MOVHMOD=${OUT}locmod.nii.gz
 for i in  $MOVH
 do
 PRE=$OUT
-TOUT=${PRE}locmod.nii
+TOUT=${PRE}locmod.nii.gz
 echo " DOING $TOUT "
-${ANTSPATH}ThresholdImage 3 $i ${PRE}tempb.nii 30.5 32.5
-${ANTSPATH}MultiplyImages 3 $i  ${PRE}tempb.nii $TOUT
-${ANTSPATH}ThresholdImage 3 $i ${PRE}tempb.nii 20.5 22.5
-${ANTSPATH}MultiplyImages 3 $i ${PRE}tempb.nii ${PRE}tempb.nii
-${ANTSPATH}ImageMath 3 $TOUT + $TOUT ${PRE}tempb.nii
-${ANTSPATH}ThresholdImage 3 $i ${PRE}tempb.nii 8.5 10.5
-${ANTSPATH}MultiplyImages 3 $i ${PRE}tempb.nii ${PRE}tempb.nii
-${ANTSPATH}ImageMath 3 $TOUT + $TOUT ${PRE}tempb.nii
+${ANTSPATH}ThresholdImage 3 $i ${PRE}tempb.nii.gz 30.5 32.5
+${ANTSPATH}MultiplyImages 3 $i  ${PRE}tempb.nii.gz $TOUT
+${ANTSPATH}ThresholdImage 3 $i ${PRE}tempb.nii.gz 20.5 22.5
+${ANTSPATH}MultiplyImages 3 $i ${PRE}tempb.nii.gz ${PRE}tempb.nii
+${ANTSPATH}ImageMath 3 $TOUT + $TOUT ${PRE}tempb.nii.gz
+${ANTSPATH}ThresholdImage 3 $i ${PRE}tempb.nii.gz 8.5 10.5
+${ANTSPATH}MultiplyImages 3 $i ${PRE}tempb.nii.gz ${PRE}tempb.nii.gz
+${ANTSPATH}ImageMath 3 $TOUT + $TOUT ${PRE}tempb.nii.gz
 done
 else
 FIXHMOD=$FIXH
@@ -72,7 +73,7 @@ OUT=$7
 fi
 
 STEPL=0.25
-INTENSITY=PR[$FIX,${MOV},${INTWT},2]
+INTENSITY=PR[$FIX,${MOV},${INTWT},4]
 if [ $LMWT -le 0  ]
 then
 exe="${ANTSPATH}ANTS 3  -o $OUT  -i $ITS -t SyN[${STEPL}]  -r Gauss[3,0]   -m $INTENSITY   "
@@ -83,6 +84,10 @@ echo " $exe "
 
  $exe
 
- ${ANTSPATH}WarpImageMultiTransform 3 $MOV ${OUT}deformed.nii  ${OUT}Warp.nii ${OUT}Affine.txt  -R $FIX
- ${ANTSPATH}WarpImageMultiTransform 3 $MOVH  ${OUT}label.nii   ${OUT}Warp.nii ${OUT}Affine.txt  -R $FIX --use-NN
- ${ANTSPATH}WarpImageMultiTransform 3 $FIXH ${OUT}labelinv.nii  -i  ${OUT}Affine.txt  ${OUT}InverseWarp.nii   -R $MOV --use-NN
+ ${ANTSPATH}WarpImageMultiTransform 3 $MOV ${OUT}deformed.nii.gz  ${OUT}Warp.nii.gz ${OUT}Affine.txt  -R $FIX
+ ${ANTSPATH}WarpImageMultiTransform 3 $MOVH  ${OUT}label.nii.gz   ${OUT}Warp.nii.gz ${OUT}Affine.txt  -R $FIX --use-NN
+ ${ANTSPATH}WarpImageMultiTransform 3 $FIXH ${OUT}labelinv.nii.gz  -i  ${OUT}Affine.txt  ${OUT}InverseWarp.nii.gz   -R $MOV --use-NN
+
+
+
+
