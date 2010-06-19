c2c97f39ff970752bc2d8d3bd3bf244f241a7d88
Brian Avants <stnava@gmail.com>
2010-06-19 15:59:34 +0000

    Adding most of the necessary stuff for a basic app with decent user interface.

--- Examples/ANTSConformalMapping.cxx
+++ Examples/ANTSConformalMapping.cxx
@@ -3,13 +3,17 @@
 #include "itkImage.h"
 #include "itkImageFileReader.h"
 #include "itkImageFileWriter.h"
-#include "vtkPolyDataWriter.h"
-#include "vtkPolyDataReader.h"
 #include "itkFEM.h"
 #include "itkFEMLinearSystemWrapperItpack.h"
 #include "itkFEMElement3DC0LinearTriangularLaplaceBeltrami.h"
 #include "itkFEMElement3DC0LinearTriangularMembrane.h"
 #include "itkFEMDiscConformalMap.h"
+
+#include "vtkCallbackCommand.h"
+#include "vtkPolyDataWriter.h"
+#include "vtkPolyDataReader.h"
+#include <vtkSmartPointer.h>
+#include <vtkWindowedSincPolyDataFilter.h>
 
 #include <string>
 #include <algorithm>
@@ -52,32 +56,6 @@ public:
   }
 };
 
-/** inflation
-  std::cout << " read " << std::string(filename) << " write " << outfn << std::endl;
-  vtkPolyDataReader *fltReader = vtkPolyDataReader::New();
-  fltReader->SetFileName(filename);
-  fltReader->Update();
-  vtkSmartPointer<vtkWindowedSincPolyDataFilter> smoother =
-    vtkSmartPointer<vtkWindowedSincPolyDataFilter>::New();
-  smoother->SetInput(fltReader->GetOutput());
-  smoother->SetNumberOfIterations( (int) param );
-  smoother->BoundarySmoothingOn();
-  smoother->FeatureEdgeSmoothingOff();
-  smoother->SetFeatureAngle(180.0);
-  smoother->SetEdgeAngle(180.0);
-  smoother->SetPassBand(1.e-3); // smaller values increase smoothing
-  smoother->NonManifoldSmoothingOn();
-  smoother->NormalizeCoordinatesOff();
-  smoother->Update();
-
-  vtkPolyDataWriter *writer = vtkPolyDataWriter::New();
-  writer->SetInput(	smoother->GetOutput() );
-  writer->SetFileName(outfn.c_str());
-  writer->SetFileTypeToBinary();
-  writer->Update();
-  std::cout << " done writing ";
-*/
-
 void ConvertToLowerCase( std::string& str )
 {
   std::transform( str.begin(), str.end(), str.begin(), (int (*)(int) )tolower );
@@ -102,15 +80,78 @@ int ANTSConformalMapping( itk::CommandLineParser *parser )
   flattener->SetSigma(1);
   //  flattener->SetSurfaceMesh(vtkmesh);
 
+  // first find out if the user wants to inflate the mesh ...
+  unsigned int inflate_iterations = 0;
+  float        inflate_param = 0;
+  typename itk::CommandLineParser::OptionType::Pointer infOption =
+    parser->GetOption( "inflate" );
+  if( infOption && infOption->GetNumberOfValues() > 0 )
+    {
+    if( infOption->GetNumberOfParameters() == 2 )
+      {
+      inflate_param = parser->Convert<float>(infOption->GetParameter( 0 ) );
+      inflate_iterations = parser->Convert<unsigned int>(infOption->GetParameter( 1 ) );
+      std::cout << " you will inflate before flattening with params " << inflate_param << " applied over  "
+                << inflate_iterations << " iterations. " <<  std::endl;
+      }
+    else
+      {
+      std::cerr << " wrong params for inflation. ignoring. " << std::endl;
+      std::cerr << "   " << infOption->GetDescription() << std::endl;
+      return EXIT_FAILURE;
+      }
+    }
+
+  typename itk::CommandLineParser::OptionType::Pointer displayOption = parser->GetOption( "display-mesh" );
+  if( displayOption && displayOption->GetNumberOfValues() > 0 )
+    {
+    if( displayOption->GetNumberOfParameters() > 0 )
+      {
+      std::string dispm = displayOption->GetParameter( 0 );
+      std::cout << " render " << dispm << std::endl;
+      // read the vtk file ...
+      vtkPolyDataReader *fltReader = vtkPolyDataReader::New();
+      fltReader->SetFileName(dispm.c_str() );
+      fltReader->Update();
+
+      vtkRenderer*     ren1 = vtkRenderer::New();
+      vtkRenderWindow* renWin = vtkRenderWindow::New();
+      renWin->AddRenderer(ren1);
+      vtkRenderWindowInteractor* inter = vtkRenderWindowInteractor::New();
+      inter->SetRenderWindow(renWin);
+      vtkCallbackCommand *cbc = vtkCallbackCommand::New();
+      ren1->AddObserver(vtkCommand::KeyPressEvent, cbc);
+
+      vtkDataSetMapper* mapper = vtkDataSetMapper::New();
+      mapper->SetInput(fltReader->GetOutput() );
+      mapper->SetScalarRange(0, 255);
+      vtkActor* actor = vtkActor::New();
+      actor->SetMapper(mapper);
+      ren1->SetViewport(0.0, 0.0, 1.0, 1.0);
+      ren1->AddActor(actor);
+
+      renWin->Render();
+      inter->Start();
+
+      mapper->Delete();
+      actor->Delete();
+      ren1->Delete();
+      renWin->Delete();
+      inter->Delete();
+      return 0;
+      }
+    }
+
+  exit(1);
   /**
    * Initialization
    */
   typename itk::CommandLineParser::OptionType::Pointer initializationOption =
-    parser->GetOption( "initialization" );
+    parser->GetOption( "input-mesh" );
   if( initializationOption
-      && initializationOption->GetNumberOfParameters() < 1 )
+      && initializationOption->GetNumberOfParameters() < 2 )
     {
-    std::cerr << "Incorrect initialization option specification." << std::endl;
+    std::cerr << "Incorrect input mesh specification." << std::endl;
     std::cerr << "   " << initializationOption->GetDescription() << std::endl;
     return EXIT_FAILURE;
     }
@@ -120,9 +161,34 @@ int ANTSConformalMapping( itk::CommandLineParser *parser )
      * intensity images
      */
     typename itk::CommandLineParser::OptionType::Pointer imageOption =
-      parser->GetOption( "intensity-image" );
-    if( imageOption && imageOption->GetNumberOfValues() > 0 )
+      parser->GetOption( "input-mesh" );
+    if(  imageOption->GetNumberOfValues() > 0 )
       {
+/** inflation
+  std::cout << " read " << std::string(filename) << " write " << outfn << std::endl;
+  vtkPolyDataReader *fltReader = vtkPolyDataReader::New();
+  fltReader->SetFileName(filename);
+  fltReader->Update();
+  vtkSmartPointer<vtkWindowedSincPolyDataFilter> smoother =
+    vtkSmartPointer<vtkWindowedSincPolyDataFilter>::New();
+  smoother->SetInput(fltReader->GetOutput());
+  smoother->SetNumberOfIterations( (int) inflate_iterations );
+  smoother->BoundarySmoothingOn();
+  smoother->FeatureEdgeSmoothingOff();
+  smoother->SetFeatureAngle(180.0);
+  smoother->SetEdgeAngle(180.0);
+  smoother->SetPassBand( inflate_param ); // smaller values increase smoothing
+  smoother->NonManifoldSmoothingOn();
+  smoother->NormalizeCoordinatesOff();
+  smoother->Update();
+
+  vtkPolyDataWriter *writer = vtkPolyDataWriter::New();
+  writer->SetInput(	smoother->GetOutput() );
+  writer->SetFileName(outfn.c_str());
+  writer->SetFileTypeToBinary();
+  writer->Update();
+  std::cout << " done writing ";
+*/
       for( unsigned int n = 0; n < imageOption->GetNumberOfValues(); n++ )
         {
         typedef itk::ImageFileReader<ImageType> ReaderType;
@@ -151,13 +217,38 @@ int ANTSConformalMapping( itk::CommandLineParser *parser )
       parser->GetOption( "output" );
     if( outputOption && outputOption->GetNumberOfValues() > 0 )
       {
-      if( outputOption->GetNumberOfParameters() == 0 )
-        {
-        // WriteImage<ImageType>(img,outputOption->GetValue() ).c_str() );
-        }
       if( outputOption->GetNumberOfParameters() > 0 )
         {
         //        WriteImage<ImageType>(img,outputOption->GetParameter( 0 ) ).c_str()  );
+        for( unsigned int p = 0; p < outputOption->GetNumberOfParameters(); p++ )
+          {
+          if( p == 0 )
+            {
+            vtkPolyDataWriter *writer = vtkPolyDataWriter::New();
+            writer->SetInput(flattener->m_DiskSurfaceMesh);
+            std::string outnm = outputOption->GetParameter( p );
+            std::cout << " writing " << outnm << std::endl;
+            writer->SetFileName(outnm.c_str() );
+            writer->SetFileTypeToBinary();
+            if( flattener->m_DiskSurfaceMesh )
+              {
+              writer->Update();
+              }
+            }
+          if( p == 1 )
+            {
+            vtkPolyDataWriter *writer = vtkPolyDataWriter::New();
+            writer->SetInput(flattener->m_ExtractedSurfaceMesh);
+            std::string outnm = outputOption->GetParameter( 1 );
+            std::cout << " writing " << outnm << std::endl;
+            writer->SetFileName(outnm.c_str() );
+            writer->SetFileTypeToBinary();
+            if( flattener->m_DiskSurfaceMesh )
+              {
+              writer->Update();
+              }
+            }
+          }
         }
       }
     }
@@ -166,6 +257,7 @@ int ANTSConformalMapping( itk::CommandLineParser *parser )
   flattener->SetParamWhileSearching(paramws);
 
   std::string canonicaldomain = parser->GetOption( "canonical-domain" )->GetValue();
+  ConvertToLowerCase( canonicaldomain );
   std::cout << " you will map to a " << canonicaldomain << std::endl;
   if( canonicaldomain == std::string("circle") )
     {
@@ -197,23 +289,47 @@ void InitializeCommandLineOptions( itk::CommandLineParser *parser )
 
     {
     std::string description =
-      std::string( "One or more scalar images is specified as input " );
+      std::string(
+        "Two mesh images are specified as input - 1. defines the label mesh.  2. defines the feature mesh.  we put the 2nd mesh's values into the flat space." );
     OptionType::Pointer option = OptionType::New();
-    option->SetLongName( "intensity-image" );
-    option->SetShortName( 'a' );
-    option->SetUsageOption( 0, "[intensityImage,<ScalarWeight>]" );
+    option->SetLongName( "input-mesh" );
+    option->SetShortName( 'i' );
+    option->SetUsageOption( 0, "[InputMesh1.vtk,<InputMesh2.vtk>]" );
     option->SetDescription( description );
     parser->AddOption( option );
     }
 
     {
     std::string description =
-      std::string( "The output consists of ...");
+      std::string( "Display the mesh." );
+    OptionType::Pointer option = OptionType::New();
+    option->SetLongName( "display-mesh" );
+    option->SetShortName( 'd' );
+    option->SetUsageOption( 0, "[InputMesh1.vtk]" );
+    option->SetDescription( description );
+    parser->AddOption( option );
+    }
 
+    {
+    std::string description =
+      std::string( "Inflation --- two params : \n 1. BandPass (smaller increases smoothing) -- e.g. 0.001. \n " )
+      + std::string( "2. number of iterations --- higher increases smoothing. " );
+    OptionType::Pointer option = OptionType::New();
+    option->SetLongName( "inflate" );
+    option->SetShortName( 'f' );
+    option->SetUsageOption( 0, "[<InverseSmoothingFactor=0.001>,<iterations=150>]" );
+    option->SetDescription( description );
+    parser->AddOption( option );
+    }
+
+    {
+    std::string description =
+      std::string(
+        "The output consists of one (or two) meshes ... 1. the flattened mesh with features mapped.  2. the extracted extrinisic mesh. ");
     OptionType::Pointer option = OptionType::New();
     option->SetLongName( "output" );
     option->SetShortName( 'o' );
-    option->SetUsageOption( 0, "[someImage,<some_Other_Optional_Images>]" );
+    option->SetUsageOption( 0, "[MyFlatVTKImage.vtk,<MyOptionalExtrinsicVTKImage.vtk>]" );
     option->SetDescription( description );
     parser->AddOption( option );
     }
