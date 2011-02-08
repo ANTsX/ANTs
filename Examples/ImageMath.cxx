cb44f49bbbbfd42eda37037dfc3558422a7b385b
Brian Avants <stnava@gmail.com>
2011-02-08 19:39:50 +0000

    ENH: ExtractVectorToComponent operation --- e.g. ImageMath 3 macaque_g.nii.gz ExtractVectorComponent macaque_rgb.nii.gz 1  --- works for 2-3-4 dimensionality images with arbitrary number n-vector-components per voxel

--- Examples/ImageMath.cxx
+++ Examples/ImageMath.cxx
@@ -2375,7 +2375,18 @@ int TensorFunctions(int argc, char *argv[])
           for( unsigned int ee = 0; ee < d4size; ee++ )
             {
             ind2[3] = ee;
-            pix6[ee] = d4img->GetPixel(ind2);
+            if( ee ==  2 )
+              {
+              pix6[2] = d4img->GetPixel(ind2);
+              }
+            else if( ee ==  3 )
+              {
+              pix6[3] = d4img->GetPixel(ind2);
+              }
+            else
+              {
+              pix6[ee] = d4img->GetPixel(ind2);
+              }
             }
           }
         // ITK-way
@@ -5814,6 +5825,52 @@ int Lipschitz( int argc, char *argv[] )
 }
 
 template <unsigned int ImageDimension>
+int ExtractVectorComponent( int argc, char *argv[] )
+{
+  typedef float                                       PixelType;
+  typedef itk::VectorImage<PixelType, ImageDimension> ImageType;
+  typedef itk::Image<PixelType, ImageDimension>       RealImageType;
+  int          argct = 2;
+  std::string  outname = std::string(argv[argct]); argct++;
+  std::string  operation = std::string(argv[argct]);  argct++;
+  std::string  inname = std::string(argv[argct]);   argct++;
+  unsigned int whichvec = atoi(argv[argct]);   argct++;
+  typedef itk::ImageFileReader<ImageType> ReaderType;
+  typename ReaderType::Pointer reader1 = ReaderType::New();
+  reader1->SetFileName( inname.c_str() );
+  reader1->Update();
+  typename ImageType::Pointer vecimage = reader1->GetOutput();
+  if( whichvec >= vecimage->GetVectorLength() )
+    {
+    std::cout << " input image " << inname << " only has " << vecimage->GetVectorLength() << " components "
+              << std::endl;
+    return EXIT_FAILURE;
+    }
+  else
+    {
+    typename RealImageType::Pointer component = RealImageType::New();
+    component->SetOrigin( vecimage->GetOrigin() );
+    component->SetSpacing( vecimage->GetSpacing() );
+    component->SetRegions( vecimage->GetLargestPossibleRegion() );
+    component->SetDirection(  vecimage->GetDirection() );
+    component->Allocate();
+    component->FillBuffer(0);
+    typedef itk::ImageRegionIteratorWithIndex<ImageType> Iterator;
+    Iterator It1( vecimage, vecimage->GetLargestPossibleRegion() );
+    for( It1.GoToBegin(); !It1.IsAtEnd(); ++It1 )
+      {
+      component->SetPixel(It1.GetIndex(), It1.Get()[whichvec]);
+      }
+    typedef itk::ImageFileWriter<RealImageType> RealImageWriterType;
+    typename RealImageWriterType::Pointer realwriter = RealImageWriterType::New();
+    realwriter->SetFileName( outname.c_str() );
+    realwriter->SetInput( component );
+    realwriter->Update();
+    }
+  return EXIT_SUCCESS;
+}
+
+template <unsigned int ImageDimension>
 int InvId( int argc, char *argv[] )
 {
   std::cout << " Compute  phi(  phi^{-1}(x)) " << std::endl;
@@ -6940,6 +6997,8 @@ int main(int argc, char *argv[])
       <<
     "  TensorToVectorComponent DTImage WhichVec --- 0 => 2 produces component of the principal vector field , i.e. largest eigenvalue.   3 = 8 => gets values from the tensor "
       << std::endl;
+    std::cout << "  ExtractVectorComponent VecImage WhichVec ---  produces the WhichVec component of the vector "
+              << std::endl;
     std::cout
       << "  TensorIOTest DTImage --- will write the DT image back out ... tests I/O processes for consistency. "
       << std::endl;
@@ -7128,6 +7187,10 @@ int main(int argc, char *argv[])
       else if( strcmp(operation.c_str(), "GetLargestComponent") == 0 )
         {
         GetLargestComponent<2>(argc, argv);
+        }
+      else if( strcmp(operation.c_str(), "ExtractVectorComponent") == 0 )
+        {
+        ExtractVectorComponent<2>(argc, argv);
         }
       else if( strcmp(operation.c_str(), "ThresholdAtMean") == 0 )
         {
@@ -7355,6 +7418,10 @@ int main(int argc, char *argv[])
       else if( strcmp(operation.c_str(), "GetLargestComponent") == 0 )
         {
         GetLargestComponent<3>(argc, argv);
+        }
+      else if( strcmp(operation.c_str(), "ExtractVectorComponent") == 0 )
+        {
+        ExtractVectorComponent<3>(argc, argv);
         }
       else if( strcmp(operation.c_str(), "ThresholdAtMean") == 0 )
         {
@@ -7616,6 +7683,10 @@ int main(int argc, char *argv[])
       else if( strcmp(operation.c_str(), "GetLargestComponent") == 0 )
         {
         GetLargestComponent<4>(argc, argv);
+        }
+      else if( strcmp(operation.c_str(), "ExtractVectorComponent") == 0 )
+        {
+        ExtractVectorComponent<4>(argc, argv);
         }
       else if( strcmp(operation.c_str(), "ThresholdAtMean") == 0 )
         {
