375c80385a9ee0d00439ac800e2169042818ed09
Brian Avants <stnava@gmail.com>
2011-05-27 20:22:05 +0000

    BUG: needed to check validity of output tensor

--- Tensor/itkPreservationOfPrincipalDirectionTensorReorientationImageFilter.cxx
+++ Tensor/itkPreservationOfPrincipalDirectionTensorReorientationImageFilter.cxx
@@ -358,6 +358,14 @@ PreservationOfPrincipalDirectionTensorReorientationImageFilter<TTensorImage, TVe
       {
       outTensor = this->ApplyReorientation( localDeformation, inTensor );
       }
+    // valid values?
+    for( unsigned int jj = 0; jj < 6; jj++ )
+      {
+      if( vnl_math_isnan( outTensor[jj] ) || vnl_math_isinf( outTensor[jj]) )
+        {
+        outTensor[jj] = 0;
+        }
+      }
 
     outputIt.Set( outTensor );
     }
