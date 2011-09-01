d561b79222cd6d03ddaf9f448cc3f985f14233f1
Nick Tustison <ntustison@gmail.com>
2011-09-01 17:50:12 +0000

    BUG:  Minor output improvements.

--- Examples/CreateDTICohort.cxx
+++ Examples/CreateDTICohort.cxx
@@ -765,7 +765,7 @@ int CreateDTICohort( itk::ants::CommandLineParser *parser )
     else
       {
       std::string which;
-      if( n < numberOfControls )
+      if( n <= numberOfControls )
         {
         which = std::string( "Control" );
         }
