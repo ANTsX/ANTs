b685981de6618b94ce5cadaef286d9d8f68df37f
Nick Tustison <ntustison@gmail.com>
2012-05-22 11:50:42 +0000

    BUG:  Initialize with true.

--- Examples/antsRegistration.cxx
+++ Examples/antsRegistration.cxx
@@ -586,7 +586,7 @@ DoRegistration(typename ParserType::Pointer & parser)
       }
     regHelper->SetUseHistogramMatching(doHistogramMatch);
 
-    bool                doEstimateLearningRateAtEachIteration = false;
+    bool                doEstimateLearningRateAtEachIteration = true;
     OptionType::Pointer rateOption = parser->GetOption( "use-estimate-learning-rate-once" );
     if( rateOption && rateOption->GetNumberOfValues() > 0 )
       {
