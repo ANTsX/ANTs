09aabd30fba2de7ab5acb5630b239adbc228a7bf
Hans J. Johnson <hans-johnson@uiowa.edu>
2012-07-06 20:01:13 +0000

    ENH: ITKv4 registration patches incorporated.
    Include many patches that were in gerrit staging waiting
    for ITKv4.2 tag to be finalized.

--- SuperBuild/External_ITKv4.cmake
+++ SuperBuild/External_ITKv4.cmake
@@ -73,7 +73,7 @@ if(NOT DEFINED ${extProjName}_DIR AND NOT ${USE_SYSTEM_${extProjName}})
     )
   ### --- End Project specific additions
   set(${proj}_REPOSITORY ${git_protocol}://itk.org/ITK.git CACHE STRING "" FORCE)
-  set(${proj}_GIT_TAG 097e9e6a55f7843343c94bc080681a5c1f888b7d CACHE STRING "" FORCE) #2012-06-27
+  set(${proj}_GIT_TAG f2ec79de1ae03478aaa9a33e1f220b20f746646b CACHE STRING "" FORCE) #2012-07-06
   ExternalProject_Add(${proj}
     GIT_REPOSITORY ${${proj}_REPOSITORY}
     GIT_TAG ${${proj}_GIT_TAG}
