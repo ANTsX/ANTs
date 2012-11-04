afc75bc145a4a09c2016ce4a7271f6c6d9e427ef
Hans J. Johnson <hans-johnson@uiowa.edu>
2012-11-04 17:35:09 +0000

    ENH: Improve the use of add_test
    The add_test cmake directive has had many improvement
    to facilitate mutliple builds from the same directory.
    The changes provided here improve the compatibiltiy and
    robustness of running tests when both Release|Debug
    can come from the same build.

--- Examples/TestSuite/ANTS_ROT_GSYN_test.cmake
+++ Examples/TestSuite/ANTS_ROT_GSYN_test.cmake
@@ -1,34 +1,32 @@
 #####################################################################################
 #####################################################################################
-set(THIS_TEST_NAME ${THIS_TEST_NAME})
+set(THIS_TEST_NAME ANTS_ROT_GSYN)
 set(OUTPUT_PREFIX ${CMAKE_BINARY_DIR}/TEST_${THIS_TEST_NAME} )
 set(WARP ${OUTPUT_PREFIX}Warp.nii.gz ${OUTPUT_PREFIX}Affine.txt )
 set(INVERSEWARP -i ${OUTPUT_PREFIX}Affine.txt ${OUTPUT_PREFIX}InverseWarp.nii.gz )
 set(WARP_IMAGE ${CMAKE_BINARY_DIR}/${THIS_TEST_NAME}_warped.nii.gz)
 set(INVERSEWARP_IMAGE ${CMAKE_BINARY_DIR}/${THIS_TEST_NAME}_inversewarped.nii.gz)
 
-add_test(${THIS_TEST_NAME} ${TEST_BINARY_DIR}/ANTS 3
+add_test(NAME ${THIS_TEST_NAME} COMMAND $<TARGET_FILE:ANTS> 3
  -m MSQ[${ROT_REF_IMAGE},${ROT_MOV_IMAGE},1,0]
  -t SyN[0.25] -i 50x5x0 -r Gauss[3,0.0,32]
  -o ${OUTPUT_PREFIX}.nii.gz)
 
-add_test(${THIS_TEST_NAME}_WARP ${TEST_BINARY_DIR}/WarpImageMultiTransform 3
+add_test(NAME ${THIS_TEST_NAME}_WARP COMMAND $<TARGET_FILE:WarpImageMultiTransform> 3
  ${ROT_MOV_IMAGE} ${WARP_IMAGE} -R ${ROT_REF_IMAGE} ${WARP} )
 set_property(TEST ${THIS_TEST_NAME}_WARP APPEND PROPERTY DEPENDS ${THIS_TEST_NAME})
 
-add_test(${THIS_TEST_NAME}_WARP_METRIC_0
- ${TEST_BINARY_DIR}/MeasureImageSimilarity 3 0
+add_test(NAME ${THIS_TEST_NAME}_WARP_METRIC_0 COMMAND $<TARGET_FILE:MeasureImageSimilarity> 3 0
  ${ROT_REF_IMAGE} ${WARP_IMAGE}
  ${OUTPUT_PREFIX}log.txt ${OUTPUT_PREFIX}metric.nii.gz
  35.4473 0.5)
 set_property(TEST ${THIS_TEST_NAME}_WARP_METRIC_0 APPEND PROPERTY DEPENDS ${THIS_TEST_NAME}_WARP)
 
-add_test(${THIS_TEST_NAME}_INVERSEWARP ${TEST_BINARY_DIR}/WarpImageMultiTransform 3
+add_test(NAME ${THIS_TEST_NAME}_INVERSEWARP COMMAND $<TARGET_FILE:WarpImageMultiTransform> 3
  ${ROT_REF_IMAGE} ${INVERSEWARP_IMAGE} -R ${ROT_MOV_IMAGE} ${INVERSEWARP})
 set_property(TEST ${THIS_TEST_NAME}_INVERSEWARP APPEND PROPERTY DEPENDS ${THIS_TEST_NAME})
 
-add_test(${THIS_TEST_NAME}_INVERSEWARP_METRIC_0
- ${TEST_BINARY_DIR}/MeasureImageSimilarity 3 0
+add_test(NAME ${THIS_TEST_NAME}_INVERSEWARP_METRIC_0 COMMAND $<TARGET_FILE:MeasureImageSimilarity> 3 0
  ${ROT_MOV_IMAGE} ${INVERSEWARP_IMAGE}
  ${OUTPUT_PREFIX}log.txt ${OUTPUT_PREFIX}metric.nii.gz
  34.8184 0.5)
--- Examples/TestSuite/ANTS_SYN_WITH_TIME_test.cmake
+++ Examples/TestSuite/ANTS_SYN_WITH_TIME_test.cmake
@@ -8,17 +8,16 @@ set(WARP_IMAGE ${CMAKE_BINARY_DIR}/${THIS_TEST_NAME}_warped.nii.gz)
 set(INVERSEWARP_IMAGE ${CMAKE_BINARY_DIR}/${THIS_TEST_NAME}_inversewarped.nii.gz)
 
 
-add_test(${THIS_TEST_NAME} ${TEST_BINARY_DIR}/ANTS 2
+add_test(NAME ${THIS_TEST_NAME} COMMAND $<TARGET_FILE:ANTS> 2
  -m MSQ[${CHALF_IMAGE},${C_IMAGE},1,0.] -r Gauss[0.5,0.1]
  -t SyN[1,10,0.05] -i 150x100x2x2 -o ${OUTPUT_PREFIX}
  --geodesic 1 --number-of-affine-iterations 0)
 
-add_test(${THIS_TEST_NAME}_WARP ${TEST_BINARY_DIR}/WarpImageMultiTransform 2
+add_test(NAME ${THIS_TEST_NAME}_WARP COMMAND $<TARGET_FILE:WarpImageMultiTransform> 2
  ${C_IMAGE} ${OUTPUT_PREFIX}.nii.gz ${OUTPUT_PREFIX}Warp.nii.gz -R ${CHALF_IMAGE} )
 set_property(TEST ${THIS_TEST_NAME}_WARP APPEND PROPERTY DEPENDS ${THIS_TEST_NAME})
 
-add_test(${THIS_TEST_NAME}_WARP_METRIC_0
- ${TEST_BINARY_DIR}/MeasureImageSimilarity 2 0
+add_test(NAME ${THIS_TEST_NAME}_WARP_METRIC_0 COMMAND $<TARGET_FILE:MeasureImageSimilarity> 2 0
  ${CHALF_IMAGE} ${OUTPUT_PREFIX}.nii.gz
  ${OUTPUT_PREFIX}log.txt ${OUTPUT_PREFIX}metric.nii.gz
  0.0943736 0.1)
