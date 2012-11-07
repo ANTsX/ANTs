4f5bba5bdd62c6fe043e53deaed79555039769e9
Hans J. Johnson <hans-johnson@uiowa.edu>
2012-11-07 14:16:51 +0000

    Revert "ENH: use collapsed linear transorms"
    This reverts commit 5509f7b6d8027648f50fa32a00cf2198fc6930e4.

--- Scripts/newAntsExample.sh
+++ Scripts/newAntsExample.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #
-dim=2 # image dimensionality
+dim=3 # image dimensionality
 AP="" # /home/yourself/code/ANTS/bin/bin/  # path to ANTs binaries
 ITK_GLOBAL_DEFAULT_NUMBER_OF_THREADS=2  # controls multi-threading
 export ITK_GLOBAL_DEFAULT_NUMBER_OF_THREADS
@@ -18,11 +18,11 @@ $reg -d $dim -r [ $f, $m ,1]  \
                          -c [10000x1000x1000,1.e-8,20]  \
                         -s 4x2x1  \
                         -f 6x4x2 -l 1 \
-                        -m cc[  $f, $m , 1 , 2 ] \
+                        -m mattes[  $f, $m , 1 , 32 ] \
                          -t syn[ .3, 3, 0.0 ] \
-                         -c [100x50x50,1.e-8,20]  \
+                         -c [100x0x0,1.e-8,20]  \
                         -s 2x1x0  \
-                        -f 4x2x1 -l 1 -u 1 -b 0 -z 1 \
+                        -f 4x2x1 -l 1 -u 1 \
                        -o [${nm},${nm}_diff.nii.gz,${nm}_inv.nii.gz]
-echo CORRELATION `ImageMath $dim x NeighborhoodCorrelation $f ${nm}_diff.nii.gz`
-${AP}antsApplyTransforms -d $dim -i $m -r $f -n linear -t ${nm}1Warp.nii.gz -t ${nm}0MatrixOffset.mat -o ${nm}_warped.nii.gz
+
+${AP}antsApplyTransforms -d $dim -i $m -r $f -n linear -t ${nm}1Warp.nii.gz -t ${nm}0Affine.mat ${nm}DerivedInitialMovingTranslation.mat -o ${nm}_warped.nii.gz
