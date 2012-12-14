cb99e92f1ceb576705cdffdf99490cf0d48569e0
Brian Avants <stnava@gmail.com>
2012-12-14 20:10:16 +0000

    WIP: testing solver for sccan prior

--- Utilities/antsSCCANObject.hxx
+++ Utilities/antsSCCANObject.hxx
@@ -1685,8 +1685,10 @@ TRealType antsSCCANObject<TInputImage, TRealType>
     (void) this->ConjGrad(  this->m_VariatesP, lmsolv, x_i, 0, 10000 ); // A x = b
     vnl_svd<RealType> lmsolver( this->m_VariatesP, 1.e-6 );
     lmsolv = lmsolver.solve( x_i );
+    ::ants::antscout << "lmsolv " << lmsolv.mean() << std::endl;
     this->m_Intercept = this->ComputeIntercept(  this->m_VariatesP, lmsolv, x_i );
     VectorType x_recon = ( this->m_VariatesP * lmsolv + this->m_Intercept );
+    ::ants::antscout << "x_recon " << x_recon.mean() << std::endl;
     icept( a ) = this->m_Intercept;
     onenorm += x_i.one_norm() / this->m_MatrixP.cols();
     reconerr += ( x_i - x_recon ).one_norm() / this->m_MatrixP.cols();
