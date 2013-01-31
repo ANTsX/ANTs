6cb624225fe99047b562acb1a0cb053dc98dbc50
Hans Johnson <hans-johnson@uiowa.edu>
2013-01-31 10:35:27 -0600

    BUG: Non-template function definitions in header
    When a function is defined in a header and it is not
    a template, then it causes link errors of duplicate
    symbol.

--- Utilities/ReadWriteImage.h
+++ Utilities/ReadWriteImage.h
@@ -23,33 +23,7 @@
 #include "itkCastImageFilter.h"
 #include <sys/stat.h>
 
-bool ANTSFileExists(std::string strFilename)
-{
-  struct stat stFileInfo;
-  bool        blnReturn;
-  int         intStat;
-
-  // Attempt to get the file attributes
-  intStat = stat(strFilename.c_str(), &stFileInfo);
-  if( intStat == 0 )
-    {
-    // We were able to get the file attributes
-    // so the file obviously exists.
-    blnReturn = true;
-    }
-  else
-    {
-    // We were not able to get the file attributes.
-    // This may mean that we don't have permission to
-    // access the folder which contains this file. If you
-    // need to do that level of checking, lookup the
-    // return values of stat which will give you
-    // more details on why stat failed.
-    blnReturn = false;
-    }
-
-  return blnReturn;
-}
+extern bool ANTSFileExists(const std::string & strFilename);
 
 // Nifti stores DTI values in lower tri format but itk uses upper tri
 // currently, nifti io does nothing to deal with this. if this changes
