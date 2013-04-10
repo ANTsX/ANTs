737e7f5dab0ab77a881417d8d9c7ae76d0b68dbb
adrienkaiser <adrien.kaiser@hotmail.fr>
2013-04-10 11:48:32 -0400

    BUG: Prevent Boost from compiling libraries (ANTS only needs the header-only components of boost)

--- SuperBuild/configureboost.cmake
+++ SuperBuild/configureboost.cmake
@@ -2,7 +2,7 @@
 # Follow the boost suggestions
 #
 execute_process(COMMAND ./bootstrap.sh --prefix=${BOOST_INSTALL_DIR}
-  --with-libraries=
+  --without-libraries=atomic,chrono,context,date_time,exception,filesystem,graph,graph_parallel,iostreams,locale,math,mpi,program_options,python,random,regex,serialization,signals,system,test,thread,timer,wave
   WORKING_DIRECTORY ${BUILD_DIR} RESULT_VARIABLE boostrap_result)
 ### NOTE:  --with-libraries= is purposefull left blank to avoid building
 ###        any of the unnecessary boost libraries.  ANTS only needs
