824cf5966a781d2f8e12e09917833f87cb752075
Nick Tustison <ntustison@gmail.com>
2013-08-31 17:33:35 -0400

    BUG:  Wrong dimension specified.

--- Examples/antsRegistration3DFloat.cxx
+++ Examples/antsRegistration3DFloat.cxx
@@ -5,7 +5,7 @@ namespace ants {
 //Instantiate the 3DFloat version
 int antsRegistration3DFloat(ParserType::Pointer & parser)
 {
-    return  DoRegistration<float, 2>( parser );
+    return  DoRegistration<float, 3>( parser );
 }
 
 } //end namespace ants
