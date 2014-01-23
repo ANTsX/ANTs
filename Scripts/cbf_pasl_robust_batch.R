e202159a15399949a53dfa9700a66e52348f01b6
Jeffrey Duda <jeff.duda@gmail.com>
2014-01-23 14:58:29 -0500

    ENH: pasl mean cbf

---
+++ Scripts/cbf_pasl_robust_batch.R
@@ -0,0 +1,24 @@
+#!/share/apps/R/R-3.0.2/bin/Rscript --vanilla --slave
+
+library( "ANTsR" )
+library("extremevalues" )
+args<-commandArgs(TRUE)
+
+aslImg <- args[1]
+m0Img <- args[2]
+outname <- args[3]
+
+#init <- cbf_pasl_robust( asl_img, m0_img )
+#cbf <- cbf_pasl_quantify( init$delta, init$m0 )
+
+pasl <- antsImageRead( aslImg, 4 )
+m0 <- antsImageRead( m0Img, 3 )
+
+pasl.processing <- aslPerfusion(pasl, moreaccurate=FALSE, m0=m0, interpolation="sinc" )
+pasl.parameters <- list( sequence="pasl", m0=m0 )
+cbf <- quantifyCBF( pasl.processing$perfusion, pasl.processing$mask, pasl.parameters )
+meancbf <- cbf$kmeancbf
+cbfOverTime <- cbf$timecbf
+print(outname)
+antsImageWrite( meancbf, outname )
+
