b64ea7252826f4ea777de5c218a314a153340df3
Nick Tustison <ntustison@gmail.com>
2014-03-20 17:22:17 -0400

    ENH:  Including itkVTKGlue Module.

--- SuperBuild/External_ITKv4.cmake
+++ SuperBuild/External_ITKv4.cmake
@@ -117,6 +117,7 @@ if(NOT DEFINED ${extProjName}_DIR AND NOT ${USE_SYSTEM_${extProjName}})
       -DKWSYS_USE_MD5:BOOL=ON # Required by SlicerExecutionModel
       -DITK_WRAPPING:BOOL=OFF #${BUILD_SHARED_LIBS} ## HACK:  QUICK CHANGE
       -DModule_MGHIO:BOOL=ON
+      -DModule_ITKVtkGlue:BOOL=ON
 
       ${${proj}_DCMTK_ARGS}
       ${${proj}_WRAP_ARGS}
