c503779b3f9d5ef15a8e52122c93d608616e4336
BBAvants <stnava@gmail.com>
2014-04-21 12:18:53 -0400

    WIP: Perona-Malik time step

--- Examples/ImageMath.cxx
+++ Examples/ImageMath.cxx
@@ -12177,6 +12177,8 @@ int PMSmoothImage(int argc, char *argv[])
   filter->SetInput( image1 );
   filter->SetNumberOfIterations( sigma );
   PixelType mytimestep = spacingsize / vcl_pow( 2 , ImageDimension+1 );
+  PixelType reftimestep = 0.99 / vcl_pow( 2 , ImageDimension+1 );
+  if ( mytimestep > reftimestep ) mytimestep = reftimestep;
   filter->SetTimeStep( mytimestep );
   filter->SetConductanceParameter( 1.0 ); // might need to change this
   filter->Update();
