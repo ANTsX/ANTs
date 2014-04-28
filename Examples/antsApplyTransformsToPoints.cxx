97fcf368c6b21433e6c41f996da6e44ce60e88da
BBAvants <stnava@gmail.com>
2014-04-28 13:03:40 -0400

    DOC: more notes on tx direction.

--- Examples/antsApplyTransformsToPoints.cxx
+++ Examples/antsApplyTransformsToPoints.cxx
@@ -210,13 +210,18 @@ static void antsApplyTransformsToPointsInitializeCommandLineOptions( itk::ants::
     {
     std::string description =
       std::string( "Currently, the only input supported is a csv file with " )
-      + std::string( "columns including x,y (2D), x,y,z (3D) or x,y,z,t (4D) column headers." )
-      + std::string( "The points should be defined in physical space." )
-      + std::string( "If in doubt how to convert coordinates from your files to the space" )
-      + std::string( "required by antsApplyTransformsToPoints try creating/drawing a simple" )
-      + std::string( "label volume with only one voxel set to 1 and all others set to 0." )
-      + std::string( "Write down the voxel coordinates. Then use ImageMaths LabelStats to find" )
-      + std::string( "out what coordinates for this voxel antsApplyTransformsToPoints is" )
+      + std::string( "columns including x,y,z,t (all 4) column headers. " )
+      + std::string( "if you dont have 4D data, still supply 4D filling in extra places with zero. " )
+      + std::string( "The points should be defined in physical space. " )
+      + std::string( "Points are transformed in the OPPOSITE direction of images, therefore " )
+      + std::string( "you should pass the inverse of what is needed to warp the images. " )
+      + std::string( "Eg if the image is warped by  Affine.mat, you should pass the inverse of Affine.mat " )
+      + std::string( "to transform points defined in the same space as the image. " )
+      + std::string( "If in doubt how to convert coordinates from your files to the space " )
+      + std::string( "required by antsApplyTransformsToPoints try creating/drawing a simple " )
+      + std::string( "label volume with only one voxel set to 1 and all others set to 0. " )
+      + std::string( "Write down the voxel coordinates. Then use ImageMaths LabelStats to find " )
+      + std::string( "out what coordinates for this voxel antsApplyTransformsToPoints is " )
       + std::string( "expecting." );
 
     OptionType::Pointer option = OptionType::New();
