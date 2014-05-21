0843395942110479601591b79258281789362e09
ntustison <ntustison@gmail.com>
2014-05-21 11:55:51 -0400

    ENH:  Moved common functions to antsUtilities.h

--- Examples/antsUtilities.h
+++ Examples/antsUtilities.h
@@ -575,6 +575,60 @@ arCastImage( typename TImageIn::Pointer Rimage )
   return caster->GetOutput();
 }
 
+
+template<class TValue>
+TValue Convert( std::string optionString )
+{
+  TValue value;
+  std::istringstream iss( optionString );
+  iss >> value;
+  return value;
+}
+
+template<class TValue>
+std::vector<TValue> ConvertVector( std::string optionString )
+{
+  std::vector<TValue> values;
+  std::string::size_type crosspos = optionString.find( 'x', 0 );
+
+  if( crosspos == std::string::npos )
+    {
+    values.push_back( Convert<TValue>( optionString ) );
+    }
+  else
+    {
+    std::string element = optionString.substr( 0, crosspos ) ;
+
+    TValue value;
+    std::istringstream iss( element );
+    iss >> value;
+    values.push_back( value );
+    while ( crosspos != std::string::npos )
+      {
+      std::string::size_type crossposfrom = crosspos;
+      crosspos = optionString.find( 'x', crossposfrom + 1 );
+      if( crosspos == std::string::npos )
+        {
+        element = optionString.substr( crossposfrom + 1, optionString.length() );
+        }
+      else
+        {
+        element = optionString.substr( crossposfrom + 1, crosspos - ( crossposfrom + 1 ) );
+        }
+
+      std::istringstream iss2( element );
+      iss2 >> value;
+      values.push_back( value );
+      }
+    }
+  return values;
+}
+
+
+
+
+
+
 } // end namespace
 
 // ##########################################################################
