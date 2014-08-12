1593a7777d0e6c8be0b9462012328bde421510b9
BBAvants <stnava@gmail.com>
2014-08-12 09:30:59 -0400

    WIP: toward v2.1.0 release

--- SuperBuild/External_ITKv4.cmake
+++ SuperBuild/External_ITKv4.cmake
@@ -150,7 +150,7 @@ if(NOT DEFINED ${extProjName}_DIR AND NOT ${USE_SYSTEM_${extProjName}})
   ### --- End Project specific additions
   set(${proj}_REPOSITORY ${git_protocol}://itk.org/ITK.git)
   set(${proj}_REPOSITORY "https://github.com/InsightSoftwareConsortium/ITK.git")
-  set(${proj}_GIT_TAG 796b9313d8b081ccf9a0f277c7f64341ad335855)
+  set(${proj}_GIT_TAG 3d95bcf22e141e64fae2ba8e48104bc467230412)
   set(ITK_VERSION_ID ITK-4.6)
 
   ExternalProject_Add(${proj}
