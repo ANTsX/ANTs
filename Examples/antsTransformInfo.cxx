d07204096a6905047d2a01a2309ea763adcd1b2c
Jeffrey Duda <jeff.duda@gmail.com>
2014-08-18 15:20:57 -0400

    ADD: Human readable info from .mat files

---
+++ Examples/antsTransformInfo.cxx
@@ -0,0 +1,87 @@
+/*=========================================================================
+*
+*  Copyright Insight Software Consortium
+*
+*  Licensed under the Apache License, Version 2.0 (the "License");
+*  you may not use this file except in compliance with the License.
+*  You may obtain a copy of the License at
+*
+*         http://www.apache.org/licenses/LICENSE-2.0.txt
+*
+*  Unless required by applicable law or agreed to in writing, software
+*  distributed under the License is distributed on an "AS IS" BASIS,
+*  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+*  See the License for the specific language governing permissions and
+*  limitations under the License.
+*
+*=========================================================================*/
+#include "antsUtilities.h"
+
+#include "antsCommandLineParser.h"
+#include "itkTransformFileReader.h"
+
+#include <sstream>
+
+namespace ants
+{
+
+
+// entry point for the library; parameter 'args' is equivalent to 'argv' in (argc,argv) of commandline parameters to
+// 'main()'
+int antsTransformInfo( std::vector<std::string> args, std::ostream * /*out_stream = NULL */ )
+{
+  // put the arguments coming in as 'args' into standard (argc,argv) format;
+  // 'args' doesn't have the command name as first, argument, so add it manually;
+  // 'args' may have adjacent arguments concatenated into one argument,
+  // which the parser should handle
+  args.insert( args.begin(), "antsTransformInfo" );
+
+  int     argc = args.size();
+  char* * argv = new char *[args.size() + 1];
+  for( unsigned int i = 0; i < args.size(); ++i )
+    {
+    // allocate space for the string plus a null character
+    argv[i] = new char[args[i].length() + 1];
+    std::strncpy( argv[i], args[i].c_str(), args[i].length() );
+    // place the null character in the end
+    argv[i][args[i].length()] = '\0';
+    }
+  argv[argc] = 0;
+  // class to automatically cleanup argv upon destruction
+  class Cleanup_argv
+  {
+public:
+    Cleanup_argv( char* * argv_, int argc_plus_one_ ) : argv( argv_ ), argc_plus_one( argc_plus_one_ )
+    {
+    }
+
+    ~Cleanup_argv()
+    {
+      for( unsigned int i = 0; i < argc_plus_one; ++i )
+        {
+        delete[] argv[i];
+        }
+      delete[] argv;
+    }
+
+private:
+    char* *      argv;
+    unsigned int argc_plus_one;
+  };
+  Cleanup_argv cleanup_argv( argv, argc + 1 );
+
+  for (int i=1; i<argc; i++) {
+  
+    std::cout << "Transform file: " << argv[i] << std::endl;
+
+    itk::TransformFileReader::Pointer reader = itk::TransformFileReader::New();
+    reader->SetFileName( argv[i] );
+    reader->Update();
+
+    std::cout << *(reader->GetTransformList()->begin()) << std::endl;
+    }
+
+  return 0;
+}
+
+} // namespace ants
