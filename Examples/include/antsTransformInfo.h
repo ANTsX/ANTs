f2a31f82bc77c8b26ea65f112a3a5837f73b5eac
Jeffrey Duda <jeff.duda@gmail.com>
2014-08-19 14:43:26 -0400

    ADD: human readable .mat summary

---
+++ Examples/include/antsTransformInfo.h
@@ -0,0 +1,12 @@
+
+#ifndef ANTSTRANSFORMINFO_H
+#define ANTSTRANSFORMINFO_H
+
+namespace ants
+{
+extern int antsTransformInfo( std::vector<std::string>,  // equivalent to argv of command line parameters to main()
+                    std::ostream* out_stream   // [optional] output stream to write
+                    );
+} // namespace ants
+
+#endif // ANTSMOTIONCORRSTATS_H
