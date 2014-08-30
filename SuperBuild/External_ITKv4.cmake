10fd052d15d5988ac145c1e44bcb624c9fdae2a4
BBAvants <stnava@gmail.com>
2014-08-30 08:36:48 -0400

    COMP: super build needed 4.7 increment

--- SuperBuild/External_ITKv4.cmake
+++ SuperBuild/External_ITKv4.cmake
@@ -151,7 +151,7 @@ if(NOT DEFINED ${extProjName}_DIR AND NOT ${USE_SYSTEM_${extProjName}})
   set(${proj}_REPOSITORY ${git_protocol}://itk.org/ITK.git)
   set(${proj}_REPOSITORY "https://github.com/InsightSoftwareConsortium/ITK.git")
   set(${proj}_GIT_TAG 5d193c91ff8fc5e85fd0b36e6eabb14f503dfb9e)
-  set(ITK_VERSION_ID ITK-4.6)
+  set(ITK_VERSION_ID ITK-4.7)
 
   ExternalProject_Add(${proj}
     GIT_REPOSITORY ${${proj}_REPOSITORY}
