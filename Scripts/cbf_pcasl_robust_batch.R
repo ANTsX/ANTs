4656d08e8ab839c9e9a2515a9b1f63e619653f80
Jeff Duda <jeff.duda@gmail.com>
2014-10-07 16:27:32 -0400

    ADD: pcasl quantification

---
+++ Scripts/cbf_pcasl_robust_batch.R
@@ -0,0 +1,19 @@
+#!/usr/bin/env Rscript
+library( "ANTsR" )
+library("extremevalues" )
+args<-commandArgs(TRUE)
+
+aslImg <- args[1]
+outname <- args[2]
+
+
+pasl <- antsImageRead( aslImg, 4 )
+
+pcasl.processing <- aslPerfusion(pasl, moreaccurate=FALSE, interpolation="linear" )
+pcasl.parameters <- list( sequence="pcasl", m0=pcasl.processing$m0 )
+cbf <- quantifyCBF( pcasl.processing$perfusion, pcasl.processing$mask, pcasl.parameters )
+meancbf <- cbf$kmeancbf
+cbfOverTime <- cbf$timecbf
+print(outname)
+antsImageWrite( meancbf, outname )
+
