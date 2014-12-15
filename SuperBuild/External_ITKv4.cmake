3c68d138457e39235b599a2da04c1e872da91c94
BBAvants <stnava@gmail.com>
2014-12-15 16:59:51 -0500

    WIP: antsr compiler errors related to itk?

--- SuperBuild/External_ITKv4.cmake
+++ SuperBuild/External_ITKv4.cmake
@@ -150,7 +150,7 @@ if(NOT DEFINED ${extProjName}_DIR AND NOT ${USE_SYSTEM_${extProjName}})
   ### --- End Project specific additions
   set(${proj}_REPOSITORY ${git_protocol}://itk.org/ITK.git)
   set(${proj}_REPOSITORY "https://github.com/InsightSoftwareConsortium/ITK.git")
-  set(${proj}_GIT_TAG e92a0da3ece69dba834b3345e9070e64c3881ed1)
+  set(${proj}_GIT_TAG 6e55ef004c197d0e0850c9a65fe553c4156d3bcf)
   set(ITK_VERSION_ID ITK-4.7)
 
   ExternalProject_Add(${proj}
