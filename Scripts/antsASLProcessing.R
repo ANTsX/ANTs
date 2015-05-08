03056e47992118c5c6ece094519bae7334336264
Ben Kandel <bkandel@chead.uphs.upenn.edu>
2015-05-08 15:34:04 -0400

    ENH: Allow Bayesian option for ASL.

--- Scripts/antsASLProcessing.R
+++ Scripts/antsASLProcessing.R
@@ -7,12 +7,12 @@ if(!usePkg('optparse') | !usePkg('ANTsR')){
 
 optlist <- list(
   make_option(c('-s', '--pCASL'), default='', help=' raw pCASL image'),
-  make_option(c('-o', '--outpre'), default='CBF_',
+  make_option(c('-o', '--outputpre'), default='CBF_',
               help='output prefix (defaults to %default)'),
   make_option(c('-t', '--antsCorticalThicknessPrefix'),
               default='', help='prefix of antsCorticalThickness output'),
-  make_option(c('-l', '--labelImage'),
-              default='', help='label image in template space to warp to ASL'),
+  make_option(c('-l', '--labelSet'),
+              default='', help='label set in template space to warp to ASL'),
   make_option(c('-c', '--paramFile'), default='',
               help='parameter file containing ASL acquisition parameters'),
   make_option(c('-m', '--method'), default='RobustRegression',
@@ -45,20 +45,19 @@ optlist <- list(
 usage <- OptionParser(option_list=optlist, usage='Usage: %prog <s> [apgxetlomdbrnckfv]')
 opt <- parse_args(usage)
 ## debug
-#opt <- data.frame(pCASL='/Users/bkandel/CfN//home/bkandel/tmp/pcasl.nii.gz',
-#antsCorticalThicknessPrefix=paste(
-#      '/Users/bkandel/CfN/data/jag/BD2K01/ASL_pipeline/',
-#      'data/AddictionCenter/ABART/',
-##      'processed/ABART_Bac_106/Anatomy/ABART_Bac_106_', sep=''),
-# labelImage='/Users/bkandel/CfN/data/jag/BD2K01/ASL_pipeline/data/AddictionCenter/template/VS_OFC.nii.gz',
-#                  outpre='test')
+#opt <- data.frame(pCASL='data/101_pcasl.nii.gz',
+#                  out='test')
 
-if(length(opt$verbose > 0)) {
-    if(opt$verbose == TRUE) {
-    cat('Running antsASLProcessing.R with the following options:\n')
-    for(option in names(opt)){
-      cat(paste(option, ': ', opt[option], '\n', sep=''))
-    }
+
+if(!file.exists(as.character(opt$pCASL))) {
+  stop(paste('pCASL image', opt$pCASL,
+    'does not exist.'))
+}
+
+if(opt$verbose) {
+  cat('Running antsASLProcessing.R with the following options:\n')
+  for(option in names(opt)){
+    cat(paste(option, ': ', opt[option], '\n', sep=''))
   }
 }
 
@@ -67,28 +66,27 @@ if(length(grep(.Platform$file.sep, opt$outputpre)) > 0) {
   if(!file.exists(outdir)) dir.create(outdir)
 }
 
-
-
 pcasl <- tryCatch({
     antsImageRead(as.character(opt$pCASL), 4)
   }, error = function(e) {
-    stop(paste('pCASL image', as.character(opt$pCASL),
+    print(paste('pCASL image', as.character(opt$pCASL),
                 'does not exist.'))
 })
 
-if(file.exists(as.character(opt$paramFile))) {
+if(length(opt$paramFile) > 0){
+  if(file.exists(as.character(opt$paramFile))) {
     config <- read.csv(opt$paramFile)
-} else {
+  } else {
     config <- data.frame(tagFirst=T, sequence='pcasl')
+  }
 }
-
 avg <- getAverageOfTimeSeries(pcasl)
 avg <- n3BiasFieldCorrection(avg, 2)
 avg <- n3BiasFieldCorrection(avg, 2)
 mask <- getMask(avg, mean(avg), Inf, 2)
 avg[mask==0] <- 0
 
-moco <- antsMotionCalculation(pcasl, moreaccurate=0)
+moco <- antsMotionCalculation(pcasl, moreaccurate=2)
 tag.first <- config$tagFirst
 ts <- timeseries2matrix(moco$moco_img, moco$moco_mask)
 if (!tag.first) {
@@ -101,8 +99,26 @@ noise.all <- cbind(moco$moco_params, moco$dvars, nuisance)
 noise.combined <- as.matrix(combineNuisancePredictors(ts, tc, noise.all))
 censored <- aslCensoring(pcasl, mask, nuis=noise.combined, method='robust')
 noise.censored <- noise.combined[censored$which.inliers, ]
-perf <- aslAveraging(censored$asl.inlier, mask=moco$moco_mask,
-                     nuisance=noise.censored, method='regression')
+if (opt$method == 'regression') {
+  perf <- aslAveraging(censored$asl.inlier, mask=moco$moco_mask,
+                       nuisance=noise.censored, method='regression')
+} else if (opt$method == 'bayesian') {
+  if (nchar(opt$antsCorticalThicknessPrefix) == 0) {
+    stop("For Bayesian regression, segmentations are required.")
+  }
+  act <- as.character(opt$antsCorticalThicknessPrefix)
+  segmentation <- tryCatch({
+        antsImageRead(paste(act, "BrainSegmentation.nii.gz", sep=""))
+      }, error = function(e) {
+        print(paste('Segmentation image', paste(act, "BrainSegmentation.nii.gz", sep=""),
+                    'does not exist.'))
+  })
+  tissuelist <- imageFileNames2ImageList(glob2rx(paste(act,
+    "BrainSegmentationPosteriors*.nii.gz", sep="")))
+  perf <- aslAveraging(censored$asl.inlier, mask=moco$moco <- mask,
+                       nuisance=noise.censored, method='bayesian',
+                       segmentation=segmentation, tissuelist=tissuelist)
+}
 
 mvals2 <- apply(ts[tc == 0.5, ], 2, mean)
 mvals1 <- apply(ts[tc == -0.5, ], 2, mean)
@@ -124,44 +140,34 @@ m0<-n3BiasFieldCorrection(m0,2)
 
 cbf <- quantifyCBF(perf, mask=moco$moco_mask,
                    parameters=list(sequence="pcasl", m0=antsImageClone(m0)))
-antsImageWrite(cbf$meancbf, paste(opt$outpre, "CBF.nii.gz", sep=""))
-antsImageWrite(perf, paste(opt$outpre, "Perfusion.nii.gz", sep=""))
+antsImageWrite(cbf$meancbf, paste(opt$outprefix, "_cbf.nii.gz", sep=""))
 
-if (nchar(opt$antsCorticalThicknessPrefix) > 0){
+if (length(opt$antsCorticalThicknessPrefix) > 0){
   act <- as.character(opt$antsCorticalThicknessPrefix)
   braint1 <- tryCatch({
       antsImageRead(paste(act, "ExtractedBrain0N4.nii.gz", sep=""))
     }, error = function(e) {
-      stop(paste('T1 brain image', paste(act, "ExtractedBrain0N4.nii.gz", sep=""),
+      print(paste('T1 brain image', paste(act, "ExtractedBrain0N4.nii.gz", sep=""),
                   'does not exist.'))
   })
-  postnames <- list.files(path=dirname(act),
-            glob2rx("*BrainSegmentationPosteriors*.nii.gz"), full.names=TRUE)
-  probs <- imageFileNames2ImageList(postnames)
+  probs <- imageFileNames2ImageList(glob2rx(paste(act,
+    "BrainSegmentationPosteriors*.nii.gz", sep="")))
   seg <- tryCatch({
       antsImageRead(paste(act, "BrainSegmentation.nii.gz", sep=""))
     }, error = function(e) {
-      stop(paste('Segmentation image', paste(act, "BrainSegmentation.nii.gz", sep=""),
+      print(paste('Segmentation image', paste(act, "BrainSegmentation.nii.gz", sep=""),
                   'does not exist.'))
   })
   reg.t12asl <- antsRegistration(fixed=avg, moving=braint1,
-    typeofTransform="SyNBold", outprefix=as.character(opt$outpre))
+    typeofTransform="SynBold", outprefix=opt$outputpre)
   seg.asl <- antsApplyTransforms(avg, seg, reg.t12asl$fwdtransforms, "MultiLabel")
-  antsImageWrite(seg.asl, paste(opt$outpre,
-                                "SegmentationWarpedToASL.nii.gz", sep=""))
-  tx.template2t1 <- c(paste(act, "SubjectToTemplate0GenericAffine.mat", sep=""),
-                      paste(act, "SubjectToTemplate1Warp.nii.gz", sep=""))
+  tx.template2t1 <- c(paste(act, "_SubjectToTemplate0GenericAffine.mat", sep=""),
+                      paste(act, "_SubjectToTemplate1Warp.nii.gz", sep=""))
   tx.template2asl <- c(tx.template2t1, reg.t12asl$invtransforms)
-  if (nchar(as.character(opt$labelImage)) > 0) {
-    label <- tryCatch( {
-      antsImageRead(as.character(opt$labelImage))
-    }, error = function(e) {
-      stop(paste("Label image", as.character(opt$labelImage),
-                 "does not exist."))
-    })
-    label.asl <- antsApplyTransforms(fixed=avg, moving=label,
-      transformlist=tx.template2asl, interpolator="MultiLabel")
-    antsImageWrite(label.asl, paste(opt$outpre,
+  if (length(opt$labelSet) > 0) {
+    label <- antsImageRead(opt$labelSet)
+    label.asl <- antsApplyTransforms(avg, label, tx.template2asl)
+    antsImageWrite(label.asl, paste(opt$outputpre,
       'LabelWarpedToASL.nii.gz', sep=''))
   }
 }
