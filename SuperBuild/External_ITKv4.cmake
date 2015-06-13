5608ed64ee9993882e99781172568238933170d6
Hans Johnson <hans-johnson@uiowa.edu>
2015-06-12 21:02:28 -0500

    BUG: ITK fix for resolution bug
    This provides a working version of ANTS
    for convering displacements to/from itk::Image
    and itk::Transform without loss of precision.
    
    Fixes https://github.com/stnava/ANTs/issues/74

--- SuperBuild/External_ITKv4.cmake
+++ SuperBuild/External_ITKv4.cmake
@@ -153,8 +153,7 @@ if(NOT DEFINED ${extProjName}_DIR AND NOT ${USE_SYSTEM_${extProjName}})
 
   ### --- End Project specific additions
   set(${proj}_REPOSITORY ${git_protocol}://itk.org/ITK.git)
-  set(${proj}_REPOSITORY "https://github.com/InsightSoftwareConsortium/ITK.git")
-  set(${proj}_GIT_TAG 5538e605e730f054d40fee78c258fbaf959fbc92)
+  set(${proj}_GIT_TAG 5538e605e730f054d40fee78c258fbaf959fbc92) ## include bug fix for float transforms!
   set(ITK_VERSION_ID ITK-4.9) ### NOTE: When updating GIT_TAG, also update ITK_VERSION_ID
 
   ExternalProject_Add(${proj}
