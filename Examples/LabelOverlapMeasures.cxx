fd89a011b2394af4dc7035e0e9e552cf6612f956
Nick Tustison <ntustison@gmail.com>
2015-07-19 08:39:58 -0700

    BUG:  issue #208

--- Examples/LabelOverlapMeasures.cxx
+++ Examples/LabelOverlapMeasures.cxx
@@ -85,9 +85,9 @@ int LabelOverlapMeasures( int argc, char * argv[] )
     rowHeaders.push_back( std::string( "All" ) );
 
     std::vector<int>::const_iterator itL = allLabels.begin();
-    std::ostringstream convert;// stream used for the conversion
     while( itL != allLabels.end() )
       {
+      std::ostringstream convert; // stream used for the conversion
       convert << *itL;   // insert the textual representation of 'Number' in the characters in the stream
       rowHeaders.push_back( convert.str() );
       ++itL;
