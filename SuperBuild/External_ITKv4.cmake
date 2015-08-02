aa936cc9637e34d4f162285c2cec73bd0800ee82
stnava <stnava@gmail.com>
2015-08-02 07:37:46 -0700

    PERF: MI thread performance from ITK

--- SuperBuild/External_ITKv4.cmake
+++ SuperBuild/External_ITKv4.cmake
@@ -153,7 +153,7 @@ if(NOT DEFINED ${extProjName}_DIR AND NOT ${USE_SYSTEM_${extProjName}})
 
   ### --- End Project specific additions
   set(${proj}_REPOSITORY ${git_protocol}://itk.org/ITK.git)
-  set(${proj}_GIT_TAG 3b7d4fde0d71e0bb01c3a5f5c64f0ff09a6962f8) ## include bug fix for float transforms!
+  set(${proj}_GIT_TAG 8f51c0dcb33e796a3aac2728a0d613527b169491) ## MI multi-thread performance
   set(ITK_VERSION_ID ITK-4.9) ### NOTE: When updating GIT_TAG, also update ITK_VERSION_ID
 
   ExternalProject_Add(${proj}
