98644cd2caa1d30f0ef7cc804098111ee680bc39
Nick Tustison <ntustison@gmail.com>
2016-04-20 19:51:58 -0700

    BUG:  Use Mattes in brain extraction.

--- Scripts/antsBrainExtraction.sh
+++ Scripts/antsBrainExtraction.sh
@@ -423,7 +423,7 @@ if [[ ! -f ${EXTRACTION_MASK} || ! -f ${EXTRACTION_WM} ]];
 
 #             exe_initial_align="${ANTSPATH}/antsAffineInitializer ${DIMENSION} ${EXTRACTION_INITIAL_AFFINE_FIXED} ${EXTRACTION_INITIAL_AFFINE_MOVING} ${EXTRACTION_INITIAL_AFFINE} 15 0.1 0 10"
             exe_initial_align="${ANTSPATH}/antsAI -d ${DIMENSION} -v 1"
-            exe_initial_align="${exe_initial_align} -m MI[${EXTRACTION_INITIAL_AFFINE_FIXED},${EXTRACTION_INITIAL_AFFINE_MOVING},32,Regular,0.25]"
+            exe_initial_align="${exe_initial_align} -m Mattes[${EXTRACTION_INITIAL_AFFINE_FIXED},${EXTRACTION_INITIAL_AFFINE_MOVING},32,Regular,0.25]"
             exe_initial_align="${exe_initial_align} -t Affine[0.1]"
             exe_initial_align="${exe_initial_align} -s [15,0.1]"
             exe_initial_align="${exe_initial_align} -p 0"
