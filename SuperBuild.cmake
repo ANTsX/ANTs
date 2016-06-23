8234565e12d1b0be855f2b624025b812680bd817
Philip A Cook <cookpa@mail.med.upenn.edu>
2016-06-23 10:15:07 -0400

    BUG: Github URLs don't work with http, need https
    Only affects users who can't use git:// URLs

--- SuperBuild.cmake
+++ SuperBuild.cmake
@@ -20,7 +20,7 @@ include(${CMAKE_CURRENT_SOURCE_DIR}/Common.cmake)
 option(${CMAKE_PROJECT_NAME}_USE_GIT_PROTOCOL "If behind a firewall turn this off to use http instead." ON)
 set(git_protocol "git")
 if(NOT ${CMAKE_PROJECT_NAME}_USE_GIT_PROTOCOL)
-  set(git_protocol "http")
+  set(git_protocol "https")
 endif()
 
 find_package(Git REQUIRED)
