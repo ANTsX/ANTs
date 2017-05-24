ANTsX/ANTs

commit 51b08bb5ab03275df81f6c7047ab7d2d59705c8a

BUG: cleanup function in bash scripts killed all matching processes
Now try to kill child processes of the invoking script

Scripts/antsRegistrationSpaceTime.sh
@@ -163,7 +163,7 @@ script by Nick Tustison
 
 HELP
     exit 1
-}
+
 
 function reportMappingParameters {
     cat <<REPORTMAPPINGPARAMETERS
@@ -187,27 +187,17 @@ REPORTMAPPINGPARAMETERS
 }
 
 cleanup()
-# example cleanup function
 {
-
-  cd ${currentdir}/
-
   echo "\n*** Performing cleanup, please wait ***\n"
-
-# 1st attempt to kill all remaining processes
-# put all related processes in array
-runningANTSpids=( `ps -C antsRegistration | awk '{ printf "%s\n", $1 ; }'` )
-
-# debug only
-  #echo list 1: ${runningANTSpids[@]}
-
-# kill these processes, skip the first since it is text and not a PID
-for (( i = 1; i < ${#runningANTSpids[@]}; i++ ))
+  
+  runningANTSpids=$( ps --ppid $$ -o pid= )
+  
+  for thePID in $runningANTSpids
   do
-    echo "killing:  ${runningANTSpids[${i}]}"
-    kill ${runningANTSpids[${i}]}
-done
-
+      echo "killing:  ${thePID}"
+      kill ${thePID}
+  done
+  
   return $?
 }
 

Scripts/antsIntroduction.sh
@@ -276,28 +276,19 @@ dataCheck
 
 }
 
+
 cleanup()
-# example cleanup function
 {
-
-  cd ${currentdir}/
-
-  echo -en "\n*** Performing cleanup, please wait ***\n"
-
-# 1st attempt to kill all remaining processes
-# put all related processes in array
-  runningANTSpids=( `ps -C ANTS -C N4BiasFieldCorrection -C antsIntroduction.sh | awk '{ printf "%s\n", $1 ; }'` )
-
-# debug only
-  #echo list 1: ${runningANTSpids[@]}
-
-# kill these processes, skip the first since it is text and not a PID
-  for ((i = 1; i < ${#runningANTSpids[@]} ; i++))
+  echo "\n*** Performing cleanup, please wait ***\n"
+  
+  runningANTSpids=$( ps --ppid $$ -o pid= )
+  
+  for thePID in $runningANTSpids
   do
-  echo "killing:  ${runningANTSpids[${i}]}"
-  kill ${runningANTSpids[${i}]}
+      echo "killing:  ${thePID}"
+      kill ${thePID}
   done
-
+  
   return $?
 }
 

Scripts/antsJointLabelFusion.sh
@@ -312,18 +312,15 @@ REPORTPARAMETERS
 cleanup()
 {
   echo "\n*** Performing cleanup, please wait ***\n"
-
-# 1st attempt to kill all remaining processes
-# put all related processes in array
-runningANTSpids=( `ps -C antsRegistration -C ImageMath| awk '{ printf "%s\n", $1 ; }'` )
-
-# kill these processes, skip the first since it is text and not a PID
-for ((i = 1; i < ${#runningANTSpids[@]} ; i++))
+  
+  runningANTSpids=$( ps --ppid $$ -o pid= )
+  
+  for thePID in $runningANTSpids
   do
-  echo "killing:  ${runningANTSpids[${i}]}"
-  kill ${runningANTSpids[${i}]}
-done
-
+      echo "killing:  ${thePID}"
+      kill ${thePID}
+  done
+  
   return $?
 }
 

Scripts/antsMalfLabeling.sh
@@ -256,21 +256,19 @@ function reportParameters {
 REPORTPARAMETERS
 }
 
+
 cleanup()
 {
   echo "\n*** Performing cleanup, please wait ***\n"
-
-# 1st attempt to kill all remaining processes
-# put all related processes in array
-runningANTSpids=( `ps -C antsRegistration -C ImageMath| awk '{ printf "%s\n", $1 ; }'` )
-
-# kill these processes, skip the first since it is text and not a PID
-for ((i = 1; i < ${#runningANTSpids[@]} ; i++))
+  
+  runningANTSpids=$( ps --ppid $$ -o pid= )
+  
+  for thePID in $runningANTSpids
   do
-  echo "killing:  ${runningANTSpids[${i}]}"
-  kill ${runningANTSpids[${i}]}
-done
-
+      echo "killing:  ${thePID}"
+      kill ${thePID}
+  done
+  
   return $?
 }
 

Scripts/antsMultivariateTemplateConstruction.sh
@@ -307,27 +307,17 @@ done
 }
 
 cleanup()
-# example cleanup function
 {
-
-  cd ${currentdir}/
-
-  echo -en "\n*** Performing cleanup, please wait ***\n"
-
-# 1st attempt to kill all remaining processes
-# put all related processes in array
-  runningANTSpids=( `ps -C ANTS -C N4BiasFieldCorrection -C ImageMath| awk '{ printf "%s\n", $1 ; }'` )
-
-# debug only
-  #echo list 1: ${runningANTSpids[@]}
-
-# kill these processes, skip the first since it is text and not a PID
-  for ((i = 1; i < ${#runningANTSpids[@]} ; i++))
+  echo "\n*** Performing cleanup, please wait ***\n"
+  
+  runningANTSpids=$( ps --ppid $$ -o pid= )
+  
+  for thePID in $runningANTSpids
   do
-  echo "killing:  ${runningANTSpids[${i}]}"
-  kill ${runningANTSpids[${i}]}
+      echo "killing:  ${thePID}"
+      kill ${thePID}
   done
-
+  
   return $?
 }
 

Scripts/antsMultivariateTemplateConstruction2.sh
@@ -427,27 +427,17 @@ for (( g = $WHICHMODALITY; g < ${#IMAGESETARRAY[@]}; g+=$NUMBEROFMODALITIES ))
 }
 
 cleanup()
-# example cleanup function
 {
-
-  cd ${currentdir}/
-
   echo "\n*** Performing cleanup, please wait ***\n"
-
-# 1st attempt to kill all remaining processes
-# put all related processes in array
-runningANTSpids=( `ps -C antsRegistration -C N4BiasFieldCorrection -C ImageMath| awk '{ printf "%s\n", $1 ; }'` )
-
-# debug only
-  #echo list 1: ${runningANTSpids[@]}
-
-# kill these processes, skip the first since it is text and not a PID
-for ((i = 1; i < ${#runningANTSpids[@]} ; i++))
+  
+    runningANTSpids=$( ps --ppid $$ -o pid= )
+  
+  for thePID in $runningANTSpids
   do
-  echo "killing:  ${runningANTSpids[${i}]}"
-  kill ${runningANTSpids[${i}]}
-done
-
+      echo "killing:  ${thePID}"
+      kill ${thePID}
+  done
+  
   return $?
 }
 

Scripts/antsRegistrationSyN.sh
@@ -218,27 +218,17 @@ REPORTMAPPINGPARAMETERS
 }
 
 cleanup()
-# example cleanup function
 {
-
-  cd ${currentdir}/
-
   echo "\n*** Performing cleanup, please wait ***\n"
-
-# 1st attempt to kill all remaining processes
-# put all related processes in array
-runningANTSpids=( `ps -C antsRegistration | awk '{ printf "%s\n", $1 ; }'` )
-
-# debug only
-  #echo list 1: ${runningANTSpids[@]}
-
-# kill these processes, skip the first since it is text and not a PID
-for (( i = 1; i < ${#runningANTSpids[@]}; i++ ))
+  
+    runningANTSpids=$( ps --ppid $$ -o pid= )
+  
+  for thePID in $runningANTSpids
   do
-    echo "killing:  ${runningANTSpids[${i}]}"
-    kill ${runningANTSpids[${i}]}
-done
-
+      echo "killing:  ${thePID}"
+      kill ${thePID}
+  done
+  
   return $?
 }
 

Scripts/antsRegistrationSyNQuick.sh
@@ -218,29 +218,20 @@ REPORTMAPPINGPARAMETERS
 }
 
 cleanup()
-# example cleanup function
 {
-
-  cd ${currentdir}/
-
   echo "\n*** Performing cleanup, please wait ***\n"
-
-# 1st attempt to kill all remaining processes
-# put all related processes in array
-runningANTSpids=( `ps -C antsRegistration | awk '{ printf "%s\n", $1 ; }'` )
-
-# debug only
-  #echo list 1: ${runningANTSpids[@]}
-
-# kill these processes, skip the first since it is text and not a PID
-for (( i = 1; i < ${#runningANTSpids[@]}; i++ ))
+  
+  runningANTSpids=$( ps --ppid $$ -o pid= )
+  
+  for thePID in $runningANTSpids
   do
-    echo "killing:  ${runningANTSpids[${i}]}"
-    kill ${runningANTSpids[${i}]}
-done
-
+      echo "killing:  ${thePID}"
+      kill ${thePID}
+  done
+  
   return $?
 }
+
 
 control_c()
 # run if user hits control-c

commit 740495bf81ac9cb19a0e3b7d7eb467b61733c325

BF: adding trailing } for Help in antsRegistrationSpaceTime.sh
bug was introduced in 51b08bb5ab03275df81f6c7047ab7d2d59705c8a

Scripts/antsRegistrationSpaceTime.sh
@@ -163,7 +163,7 @@ script by Nick Tustison
 
 HELP
     exit 1
-
+}
 
 function reportMappingParameters {
     cat <<REPORTMAPPINGPARAMETERS