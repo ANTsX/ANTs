2d2a0d4a8ef9ac55749aa04ba31d38929ae9cbfa
stnava <stnava@gmail.com>
2018-06-02 19:16:08 -0400

    ENH: allow random seed control
    via an environment variable called ANTS_RANDOM_SEED
    when implemented, this should be over-ridden by values
    that are passed via command line.

--- Examples/itkantsRegistrationHelper.h
+++ Examples/itkantsRegistrationHelper.h
@@ -824,6 +824,13 @@ private:
     typename RegistrationMethodType::Pointer registrationMethod = RegistrationMethodType::New();
     typedef typename RegistrationMethodType::OutputTransformType  RegistrationMethodTransformType;
 
+    char* antsRandomSeed = getenv( "ANTS_RANDOM_SEED" );
+    if ( antsRandomSeed != NULL )
+      {
+      registrationMethod->MetricSamplingReinitializeSeed(
+        atoi( antsRandomSeed ) );
+      }
+
     for( unsigned int n = 0; n < stageMetricList.size(); n++ )
       {
       if( !this->IsPointSetMetric( stageMetricList[n].m_MetricType ) )
