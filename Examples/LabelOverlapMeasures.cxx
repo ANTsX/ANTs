fc162f3f36fcea38131fa6a1b2322539811a598d
ntustison <ntustison@gmail.com>
2018-07-18 20:30:29 -0700

    BUG:  Revert back to unsigned int.

--- Examples/LabelOverlapMeasures.cxx
+++ Examples/LabelOverlapMeasures.cxx
@@ -34,7 +34,7 @@ int LabelOverlapMeasures( int argc, char * argv[] )
     outputCSVFormat = true;
     }
 
-  typedef int                          PixelType;
+  typedef unsigned int                          PixelType;
   typedef itk::Image<PixelType, ImageDimension> ImageType;
 
   typename ImageType::Pointer sourceImage = ImageType::New();
