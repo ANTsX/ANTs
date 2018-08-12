6150be46c6eea0693b1d1d702f1a145ef72be67c
muschellij2 <muschellij2@gmail.com>
2018-08-12 12:14:58 -0400

    fixing flatstructuring element

--- Examples/iMathFunctions1.h
+++ Examples/iMathFunctions1.h
@@ -25,20 +25,6 @@ namespace ants
 
 #define iMathGetLargestComponentMinSize 50;
 
-template <unsigned int ImageDimension>
-typename itk::FlatStructuringElement<ImageDimension>
-iMathGetFlatStructuringElement( unsigned int shape,
-                                unsigned long radius,
-                                bool radiusIsParametric,
-                                unsigned int lines,
-                                unsigned int thickness,
-                                bool includeCenter );
-#define iMathGetFlatStructuringElementShape 1;
-#define iMathGetFlatStructuringElementRadius 1;
-#define iMathGetFlatStructuringElementLines 3;
-#define iMathGetFlatStructuringElementThickness 1;
-#define iMathGetFlatStructuringElementIncludeCenter false;
-#define iMathGetFlatStructuringElementRadiusIsParametric false;
 
 
 
