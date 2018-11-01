c371bc9bf6f0b3d88397110ed075b9da4cc03c65
luz.paz <luzpaz@users.noreply.github.com>
2018-11-01 13:54:03 -0400

    WIP: Update vtk: InsertNextTypedTuple replaced InsertNextTupleValue
    per vtk 7.1 updates at https://www.vtk.org/doc/nightly/html/VTK-7-1-Changes.html
    Issue #466

--- Examples/antsSurf.cxx
+++ Examples/antsSurf.cxx
@@ -478,7 +478,7 @@ int antsImageToSurface( itk::ants::CommandLineParser *parser )
     currentColor[2] = static_cast<unsigned char>( currentBlue  * 255.0 );
     currentColor[3] = static_cast<unsigned char>( currentAlpha * 255.0 );
 
-    colors->InsertNextTupleValue( currentColor );
+    colors->InsertNextTypedTuple( currentColor );
     }
   vtkMesh->GetPointData()->SetScalars( colors );
 
