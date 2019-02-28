925d51b5361740bd8e2a55272b741ad92f94935c
muschellij2 <muschellij2@gmail.com>
2019-02-28 15:52:08 -0500

    updated the ITK tag 77156ea1f25e9d5524dc176a7e334a110b11f1a8

--- SuperBuild/External_ITKv5.cmake
+++ SuperBuild/External_ITKv5.cmake
@@ -149,7 +149,7 @@ if(NOT DEFINED ${extProjName}_DIR AND NOT ${USE_SYSTEM_${extProjName}})
   ### --- End Project specific additions
   # set(${proj}_REPOSITORY ${git_protocol}://github.com/InsightSoftwareConsortium/ITK.git)
   set(${proj}_REPOSITORY ${git_protocol}://github.com/muschellij2/ITK.git)
-  set(${proj}_GIT_TAG 7bed0ded9117c1b6bc92aab1d97d33729be2ad77 ) # eigen3 and VNL
+  set(${proj}_GIT_TAG 77156ea1f25e9d5524dc176a7e334a110b11f1a8 ) # eigen3 and VNL
   set(ITK_VERSION_ID ITK-5.0) ### NOTE: When updating GIT_TAG, also update ITK_VERSION_ID
 
   ExternalProject_Add(${proj}
