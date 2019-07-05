f7a3316b4065c9b2d34a761044432512ec1dac41
Bryn Lloyd <lloyd@itis.swiss>
2019-07-05 07:53:28 +0200

    compile errors on windows

--- Examples/CreateTiledMosaic.cxx
+++ Examples/CreateTiledMosaic.cxx
@@ -292,8 +292,8 @@ int CreateMosaic( itk::ants::CommandLineParser *parser )
   int paddingType = 0;
   RealType padValue = 0;
 
-  unsigned long lowerBound[ImageDimension];
-  unsigned long upperBound[ImageDimension];
+  ImageType::SizeValueType lowerBound[ImageDimension];
+  ImageType::SizeValueType upperBound[ImageDimension];
 
   SliceType::RegionType              croppedSliceRegion;
   SliceType::RegionType::SizeType    croppedSliceSize;
