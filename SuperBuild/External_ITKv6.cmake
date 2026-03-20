d037fe9d87f41d061a7b89bfe9927a86298024a1
mattcieslak <mattcieslak@gmail.com>
2026-03-19 21:02:51 -0400

    Final update

--- SuperBuild/External_ITKv6.cmake
+++ SuperBuild/External_ITKv6.cmake
@@ -112,8 +112,8 @@ if(NOT DEFINED ${extProjName}_DIR AND NOT ${USE_SYSTEM_${extProjName}})
   # Pin TractographyTRX/trx-cpp revisions used by ITK remote module resolution.
   # This avoids floating 'main' behavior in CI/superbuild where dependency
   # updates can break Docker builds unexpectedly.
-  set(TractographyTRX_GIT_TAG "e7c88466f22cc3811a933e59aaa32e1f7ea25faf")
-  set(TrxCpp_GIT_TAG "a6a523642267cc1208851eb1b1a79a4896dde1da")
+  set(TractographyTRX_GIT_TAG "399be51cfd51de6dadd5c9b0df4ca35752ccd023")
+  set(TrxCpp_GIT_TAG "8f54c067cd962d1d13283c5965ee35eac7d9924e")
   set(${proj}_CMAKE_OPTIONS
       -DBUILD_TESTING:BOOL=OFF
       -DBUILD_EXAMPLES:BOOL=OFF
